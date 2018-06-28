e44eb2ad16589d28a7937e93ecc287fbc182a734
Pavel Novichkov <psnovichkov@lbl.gov>
2018-06-27 18:25:28 -0700

    refactoring

--- generix/services.py
+++ generix/services.py
@@ -4,10 +4,11 @@ from elasticsearch import Elasticsearch
 from neo4j.v1 import GraphDatabase
 
 from .ontology import OntologyService
-from .validator import TermValidationService
+from .validator import TermValueValidationService
 from .indexer import SearchIndexerService
 from .search import SearchService
-from .provenance import ProvenanceService
+from .provenance import ProvenanceIndexerService, ProvenanceSearchService
+from .workspace import Workspace
 
 __PACKAGE_DIR = os.path.dirname(os.path.dirname(__file__))
 __CONFIG_FILE = os.path.join(__PACKAGE_DIR, 'var/config.json')
@@ -23,8 +24,13 @@ __neo4j_client = GraphDatabase.driver(
     auth=(__neo4j_config['user'], __neo4j_config['password']))
 
 
-indexer = SearchIndexerService(__es_client)
 ontology = OntologyService(__es_client)
-search = SearchService(__es_client)
-provenance = ProvenanceService(__neo4j_client)
-term_validator = TermValidationService()
+workspace = Workspace()
+
+es_indexer = SearchIndexerService(__es_client)
+es_search = SearchService(__es_client)
+neo_indexer = ProvenanceIndexerService()
+neo_search = ProvenanceSearchService(__neo4j_client)
+
+
+term_value_validator = TermValueValidationService()
