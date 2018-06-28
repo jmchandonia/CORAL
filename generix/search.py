969aa62915acae9a1955e2e36e3f1238c49fcb9a
Pavel Novichkov <psnovichkov@lbl.gov>
2018-06-27 18:24:04 -0700

    additional find methods, _build_query

--- generix/search.py
+++ generix/search.py
@@ -1,4 +1,6 @@
+import pandas as pd
 from .brick import BrickDescriptor, BrickDescriptorCollection
+from . import services
 
 _ES_BRICK_INDEX_NAME = 'generix-brick'
 
@@ -6,6 +8,27 @@ _ES_BRICK_INDEX_NAME = 'generix-brick'
 class SearchService:
     def __init__(self, es_client):
         self.__es_client = es_client
+
+    def _build_query(self, key_values):
+        items = []
+        for key, value in key_values.items():
+            tt, filed_name = key.split('.')
+            items.append({
+                tt: {filed_name: value}
+            })
+
+        query = {
+            'query': {
+                'constant_score': {
+                    'filter': {
+                        'bool': {
+                            'must': items
+                        }
+                    }
+                }
+            }
+        }
+        return query
 
     def _find_bricks(self, query, size=100):
         query['size'] = size
@@ -51,7 +74,7 @@ class SearchService:
         query = {
             "query": {
                 "terms": {
-                    "all_parent_term_ids": parent_term_ids
+                    "all_parent_path_term_ids": parent_term_ids
                 }
             }
         }
@@ -103,6 +126,20 @@ class SearchService:
         term_ids = [t.term_id for t in dim_type_terms]
         return self.find_dim_type_term_ids(term_ids)
 
+    def find_term_ids(self, term_ids):
+        query = {
+            "query": {
+                "terms": {
+                    "all_term_ids": term_ids
+                }
+            }
+        }
+        return BrickDescriptorCollection(self._find_bricks(query))
+
+    def find_terms(self, terms):
+        term_ids = [t.term_id for t in terms]
+        return self.find_term_ids(term_ids)
+
     def find_term_id_values(self, term_id, values):
         property = 'ont_' + '_'.join(term_id.split(':'))
         query = {
@@ -116,3 +153,43 @@ class SearchService:
 
     def find_term_values(self, term, values):
         return self.find_term_id_values(term.term_id, values)
+
+    def __term_stat(self, term_field_name):
+        query = {
+            "aggs": {
+                "term_stat": {
+                    "terms": {"field": term_field_name, "size": 10000}
+                }
+            },
+            "size": 0
+        }
+
+        term_ids = []
+        term_stats = []
+        result_set = self.__es_client.search(
+            index=_ES_BRICK_INDEX_NAME, body=query)
+        for hit in result_set['aggregations']['term_stat']['buckets']:
+            term_id = hit['key']
+            doc_count = hit['doc_count']
+            term_stats.append({
+                'Term ID': term_id,
+                'Bricks count': doc_count
+            })
+            term_ids.append(term_id)
+
+        term_ids_hash = services.ontology.ont_all.find_ids_hash(term_ids)
+
+        for term_stat in term_stats:
+            term_id = term_stat['Term ID']
+            term_stat['Term Name'] = term_ids_hash[term_id].term_name
+
+        return pd.DataFrame(term_stats)[['Term Name', 'Term ID', 'Bricks count']]
+
+    def data_type_terms(self):
+        return self.__term_stat('data_type_term_id')
+
+    def dim_type_terms(self):
+        return self.__term_stat('dim_type_term_ids')
+
+    def value_type_terms(self):
+        return self.__term_stat('value_type_term_id')
