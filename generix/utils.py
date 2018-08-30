aaa34e066d74b0b9bd425601dbd304c85d79954d
Pavel Novichkov <psnovichkov@lbl.gov>
2018-08-29 23:35:48 -0700

    parse_term method

--- generix/utils.py
+++ generix/utils.py
@@ -1,4 +1,5 @@
 import re
+from .ontology import Term
 
 __TERM_PATTERN = re.compile('(.+)<(.+)>')
 
@@ -6,3 +7,10 @@ __TERM_PATTERN = re.compile('(.+)<(.+)>')
 def check_term_format(value):
     m = __TERM_PATTERN.findall(value)
     return m is not None
+
+
+def parse_term(value):
+    m = __TERM_PATTERN.findall(value)
+    if m:
+        term = Term(m[0][1].strip(), term_name=m[0][0].strip())
+    return term
