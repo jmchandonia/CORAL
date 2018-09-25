ed6ea9d3ba5c51fc75cab183505c7ff6130a0917
Pavel Novichkov <psnovichkov@lbl.gov>
2018-09-25 14:03:54 -0700

    index bricks; get_type_names

--- generix/es_service.py
+++ generix/es_service.py
@@ -1,4 +1,6 @@
 from .utils import parse_term
+from .brick import BrickIndexDocumnet
+from .typedef import TYPE_NAME_BRICK
 
 
 ES_INDEX_NAME_PREFIX = 'generix-data-'
@@ -20,6 +22,104 @@ def to_es_type_name(type_name):
 class ElasticSearchService:
     def __init__(self, es_client):
         self.__es_client = es_client
+
+    def create_brick_index(self):
+        es_type = to_es_type_name(TYPE_NAME_BRICK)
+        index_name = ES_INDEX_NAME_PREFIX + es_type
+
+        settings = {
+            "settings": {
+                "analysis": {
+                    "analyzer": {
+                        "keyword": {
+                            "type": "custom",
+                            "tokenizer": "keyword"
+                        }
+                    }
+                }
+            },
+            "mappings": {
+                es_type: {
+                    "properties": {
+                        "brick_id": {
+                            "type": "text",
+                            "analyzer": "keyword"
+                        },
+                        "name": {
+                            "type": "text",
+                            "analyzer": "keyword"
+                        },
+                        "description": {
+                            "type": "text",
+                            "analyzer": "standard"
+                        },
+                        "n_dimensions": {
+                            "type": "integer"
+                        },
+                        "data_type_term_id": {
+                            "type": "text",
+                            "analyzer": "keyword",
+                            "fielddata": True
+                        },
+                        "data_type_term_name": {
+                            "type": "text",
+                            "analyzer": "keyword"
+                        },
+                        "value_type_term_id": {
+                            "type": "text",
+                            "analyzer": "keyword",
+                            "fielddata": True
+                        },
+                        "value_type_term_name": {
+                            "type": "text",
+                            "analyzer": "keyword"
+                        },
+                        "dim_type_term_ids": {
+                            "type": "text",
+                            "analyzer": "keyword",
+                            "fielddata": True
+                        },
+                        "dim_type_term_names": {
+                            "type": "text",
+                            "analyzer": "keyword"
+                        },
+                        "data_size": {
+                            "type": "integer"
+                        },
+                        "dim_sizes": {
+                            "type": "integer"
+                        },
+                        "all_term_ids": {
+                            "type": "text",
+                            "analyzer": "keyword"
+                        },
+                        "all_term_ids_with_values": {
+                            "type": "text",
+                            "analyzer": "keyword"
+                        },
+                        "all_term_values": {
+                            "type": "text",
+                            "analyzer": "keyword"
+                        },
+                        "all_parent_path_term_ids": {
+                            "type": "text",
+                            "analyzer": "keyword"
+                        }
+                    }
+                }
+            }
+        }
+
+        self.__es_client.indices.create(
+            index=index_name, body=settings)
+
+    def index_brick(self, data_holder):
+        es_type = to_es_type_name(data_holder.type_name)
+        index_name = ES_INDEX_NAME_PREFIX + es_type
+        bid = BrickIndexDocumnet(data_holder.brick)
+        doc = vars(bid)
+        self.__es_client.index(
+            index=index_name, doc_type=es_type, body=doc)
 
     def index_data(self, data_holder):
         type_def = data_holder.type_def
@@ -121,3 +221,18 @@ class ElasticSearchService:
             self.__es_client.indices.delete(index=index_name)
         except:
             raise ValueError('Can not drop index %s' % index_name)
+
+    def get_type_names(self):
+        names = []
+        for name in self.__es_client.indices.get_alias(ES_INDEX_NAME_PREFIX + '*').keys():
+            name = name[len(ES_INDEX_NAME_PREFIX):]
+            names.append(name)
+        return names
+
+    def get_entity_properties(self, type_name):
+        es_type = to_es_type_name(type_name)
+        index_name = ES_INDEX_NAME_PREFIX + es_type
+        print('From get_entity_properties:', es_type, index_name)
+
+        doc = self.__es_client.indices.get_mapping(index=index_name)
+        return list(doc[index_name]['mappings'][es_type]['properties'].keys())
