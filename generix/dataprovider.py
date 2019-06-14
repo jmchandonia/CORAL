9d8bb01211268415ece650f87d51ed99351189e0
Pavel Novichkov <pnovichkov@gmail.com>
2019-06-13 22:35:55 -0700

    mongodb query style

--- generix/dataprovider.py
+++ generix/dataprovider.py
@@ -94,10 +94,14 @@ class EntityProvider:
             self.__dict__[key] = index_prop_def
 
     def find(self, criterion=None):
-        return self.query().has(criterion).find()
+        q = self.query()
+        q.has(criterion)
+        return q.find()
 
     def find_one(self, criterion=None):
-        return self.query().has(criterion).find_one()
+        q = self.query()
+        q.has(criterion)
+        return q.find_one()
 
     def query(self):
         return Query(self.__index_type_def)
@@ -115,11 +119,28 @@ class BrickProvider(EntityProvider):
         return brick
 
 FILTER_FULLTEXT = 'FULLTEXT'
-FILTER_EQ = ' == '
-FILTER_LT = ' < '
-FILTER_LE = ' <= '
-FILTER_MT = ' > '
-FILTER_ME = ' >= '
+FILTER_EQ = '=='
+FILTER_LT = '<'
+FILTER_LTE = '<='
+FILTER_GT = '>'
+FILTER_GTE = '>='
+
+
+_OPERATIONS = {
+    '=': '==',
+    '==': '==',
+    'eq': '==',
+    '>': '>',
+    'gt': '>',
+    '<': '<',
+    'lt': '<',
+    '>=': '>=',
+    'gte': '>=',
+    '<=': '<=',
+    'lte': '<=',
+    'fulltext': 'FULLTEXT',
+    'FULLTEXT': 'FULLTEXT'
+}
 
 class Query:
     def __init__(self, index_type_def):
@@ -131,70 +152,44 @@ class Query:
             key = to_var_name('PROPERTY_', index_prop_def.name)            
             self.__dict__[key] = index_prop_def
 
-    def _check_property(self, prop_name):
+    def __check_property(self, prop_name):
         key = to_var_name('PROPERTY_', prop_name)
         if key not in self.__dict__:
             raise ValueError('Unknown property %s' % prop_name)
 
-    def _add_filters(self, criterion, filter_type):
-        if type(criterion) is dict:
-            for prop in criterion:
-                prop_name = prop
-                if type(prop) is IndexPropertyDef:
-                    prop_name = prop.name
-
-                self._check_property(prop_name)
-                filters = self.__filters.get(filter_type)
-                if filters is None:
-                    filters = []
-                    self.__filters[filter_type] = filters
-                    filters.append({
-                        'name': prop_name,
-                        'value': criterion[prop]
-                    })
-        else:
+    def __add_filters(self, criterion):
+        if type(criterion) is not dict:
             print('Error: Criterion should be a dict')
 
+        for prop, operaion_value_pairs in criterion.items():
+            prop_name = prop
+            if type(prop) is IndexPropertyDef:
+                prop_name = prop.name
 
-    # def _add_es_filter(self, es_filters, criterion):
-    #     if type(criterion) is dict:
-    #         for key in criterion:
-    #             if type(criterion[key]) is list:
-    #                 prefix = 'terms'
-    #             else:
-    #                 prefix = 'term'
-    #             es_filters[prefix + '.' + key] = criterion[key]
-    #     else:
-    #         print('Error: Criterion should be a dict')
+            self.__check_property(prop_name)
+            if type(operaion_value_pairs) is not dict:
+                operaion_value_pairs = {
+                    '==': operaion_value_pairs
+                }
 
-    def eq(self, criterion):
-        self._add_filters(criterion, FILTER_EQ)
-        return self
+            for operation, value in operaion_value_pairs.items():
+                if operation not in _OPERATIONS:
+                    raise ValueError('Unknown operation: %s' % operation)
 
-    def lt(self, criterion):
-        self._add_filters(criterion, FILTER_LT)
-        return self
-
-    def le(self, criterion):
-        self._add_filters(criterion, FILTER_LE)
-        return self
-
-    def mt(self, criterion):
-        self._add_filters(criterion, FILTER_MT)
-        return self
-
-    def me(self, criterion):
-        self._add_filters(criterion, FILTER_ME)
-        return self
-
-    def fulltext(self, criterion):
-        self._add_filters(criterion, FILTER_FULLTEXT)
-        return self
+                operation = _OPERATIONS[operation]
+                filters = self.__filters.get(operation)
+                if filters is None:
+                    filters = []
+                    self.__filters[operation] = filters
+                    filters.append({
+                        'name': prop_name,
+                        'value': value
+                    })
 
     def has(self, criterion):
         if criterion is None:
             criterion = {}
-        self._add_filters(criterion, FILTER_EQ)
+        self.__add_filters(criterion)
         return self
 
     def linked_up_to(self, type_name, criterion):
@@ -219,15 +214,14 @@ class Query:
         # # return services.es_search._find_entity_ids(self.__type_name, id_field_name, es_query)
         # return services.es_search._find_entity_ids(self.__type_name, 'id', es_query)
 
-    def find(self):
-
+    def __build_aql_trio(self):
         aql_source = '@@collection'
         aql_filter = ['1==1']
         aql_bind = { '@collection': self.__index_type_def.collection_name}
 
         i = 0
         for filter_type, filters in self.__filters.items():
-            if filter_type in [FILTER_EQ, FILTER_LT, FILTER_LE, FILTER_MT, FILTER_ME]:
+            if filter_type in [FILTER_EQ, FILTER_LT, FILTER_LTE, FILTER_GT, FILTER_GTE]:
                 for ft in filters:
                     i += 1
                     aql_filter.append( 'x.%s %s @_p%s' %(ft['name'], filter_type, i) )
@@ -237,61 +231,30 @@ class Query:
                     aql_source = 'FULLTEXT(@@collection, @ft_prop_name, @ft_prop_value)'
                     aql_bind[ 'ft_prop_name' ] = ft['name']
                     aql_bind[ 'ft_prop_value' ] = ft['value']
-
         
-        aql = 'FOR x IN %s FILTER %s RETURN x' % (aql_source, ' and '.join(aql_filter) )
+        return (aql_source, aql_filter, aql_bind)
 
+    def __to_descriptor(self, row):
+        if self.__index_type_def.name == TYPE_NAME_BRICK:
+            dd = BrickDescriptor(row)
+        else:
+            dd = EntityDescriptor(self.__index_type_def, row)
+        return dd
+
+
+    def find(self, size = 100):
+        aql_source, aql_filter, aql_bind = self.__build_aql_trio()
+        aql = 'FOR x IN %s FILTER %s RETURN x' % (aql_source, ' and '.join(aql_filter) )
+        
         print('aql = ', aql)
         print('aql_bind = ', aql_bind)
 
         data_descriptors = []
-        rs = services.arango_service.find(aql, aql_bind)
+        rs = services.arango_service.find(aql, aql_bind, size)
         for row in rs:
-            if self.__index_type_def.name == TYPE_NAME_BRICK:
-                dd = BrickDescriptor(row)
-            else:
-                dd = EntityDescriptor(self.__index_type_def, row)
-            data_descriptors.append(dd)
+            data_descriptors.append(self.__to_descriptor(row))
 
         return DataDescriptorCollection(data_descriptors=data_descriptors)
-        # TODO
-        # neo_ids = set()
-
-        # # collect ids based on links
-        # for neo_filter in self.__neo_filters:
-        #     source_type = neo_filter['dtype']
-        #     source_crierion = neo_filter['criterion']
-        #     direct = neo_filter['direct']
-        #     q = Query(source_type, {})
-        #     q.has(source_crierion)
-        #     source_ids = q.find_ids()
-
-        #     source_type = 'Brick' if source_type == 'brick' else source_type[0:1].upper(
-        #     ) + source_type[1:]
-
-        #     target_type = self.__type_name
-        #     target_type = 'Brick' if target_type == 'brick' else target_type[0:1].upper(
-        #     ) + target_type[1:]
-
-        #     linked_ids = services.neo_service.find_linked_ids(
-        #         source_type, 'id', source_ids, target_type, 'id', direct=direct)
-
-        #     for linked_id in linked_ids:
-        #         neo_ids.add(linked_id)
-
-        # es_filter = {}
-        # if len(self.__neo_filters) > 0:
-        #     # id_filed_name = 'brick_id' if self.__type_name == 'brick' else 'id'
-        #     # self._add_es_filter(es_filter, {id_filed_name: list(neo_ids)})
-        #     self._add_es_filter(es_filter, {'id': list(neo_ids)})
-        # for key in self.__es_filters:
-        #     es_filter[key] = self.__es_filters[key]
-
-        # es_query = services.es_search._build_query(es_filter)
-        # if self.__type_name == 'brick':
-        #     return DataDescriptorCollection(data_descriptors=services.es_search._find_bricks(es_query))
-        # else:
-        #     return DataDescriptorCollection(data_descriptors=services.es_search._find_entities(self.__type_name, es_query))
 
     def find_one(self):
         pass
