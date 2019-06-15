0d4bb013ba2c083adae049779e4aca1b430a57dc
Pavel Novichkov <pnovichkov@gmail.com>
2019-06-14 21:26:18 -0700

    reports are implemented

--- generix/dataprovider.py
+++ generix/dataprovider.py
@@ -42,21 +42,22 @@ class DataProvider:
 
 
 class DataReports:
-    def __init__(self):
-        pass
 
     @property
     def brick_types(self):
-        return services.ontology.data_type_terms()
-
+        return self.__brick_term_stat('data_type_term_id' )
+        
     @property
     def brick_dim_types(self):
-        return services.ontology.dim_type_terms()
+        return self.__brick_term_stat('dim_type_term_ids' )
 
     @property
     def brick_data_var_types(self):
-        return services.ontology.value_type_terms()
+        return self.__brick_term_stat('value_type_term_id' )
 
+    def __brick_term_stat(self, term_id_prop_name):
+        itd = services.indexdef.get_type_def(TYPE_NAME_BRICK)
+        return services.ontology.term_stat( itd, term_id_prop_name)
 
 class GenericsProvider:
     def __init__(self):
