a09c8767611b62024459f025829125afffb8c069
Charlie Trenholm <charleshtrenholm@gmail.com>
2019-08-30 16:45:52 -0700

    add queryBuilderService functionality so simple search component

--- src/app/search/simple-search/simple-search.component.ts
+++ src/app/search/simple-search/simple-search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Select2OptionData } from 'ng2-select2';
 import { QueryBuilderService } from '../../shared/services/query-builder.service';
+import { QueryBuilder, QueryMatch, QueryParam } from '../../shared/models/QueryBuilder';
 
 @Component({
   selector: 'app-simple-search',
@@ -47,11 +48,28 @@ export class SimpleSearchComponent implements OnInit {
     width: '100%',
   }
 
+  private queryMatch: QueryMatch = new QueryMatch();
+
+  private queryBuilderObject: QueryBuilder;
+
   constructor(
     private queryBuilder: QueryBuilderService
   ) { }
 
   ngOnInit() {
+    this.queryBuilderObject = this.queryBuilder.getCurrentObject();
+  }
+
+  updateDataType(event) {
+    this.queryMatch.dataType = event.data[0].text;
+  }
+
+  updateKeywords(event) {
+    this.queryMatch.params = [new QueryParam(null, null, event.data[0].text)];
+  }
+
+  submitSearch() {
+    console.log('SIMPLE  QBO -> ', JSON.stringify(this.queryMatch));
   }
 
 }
