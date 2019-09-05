d2b38f45a1bceccbfb38f033213a59af58d44bcc
Charlie Trenholm <charleshtrenholm@gmail.com>
2019-09-05 16:00:29 -0700

    setup prototype service for plot building and connect with UI

--- src/app/plot/plot-options/plot-options.component.ts
+++ src/app/plot/plot-options/plot-options.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { ObjectGraphMapService } from '../../shared/services/object-graph-map.service';
+import { ObjectMetadata } from '../../shared/models/object-metadata';
+import { Select2OptionData } from 'ng2-select2';
 
 @Component({
   selector: 'app-plot-options',
@@ -8,14 +11,24 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class PlotOptionsComponent implements OnInit {
 
-  private dimensions = ['X', 'Y'];
+  private dimensions = ['x', 'y', 'z'];
+  private plotObject: ObjectMetadata;
+  private plotTypeOptions: Array<Select2OptionData> = [{id: '', text: ''}];
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private objectGraphMap: ObjectGraphMapService
+    ) { }
   private objectId: string;
 
   ngOnInit() {
     this.route.params.subscribe(params => {
       this.objectId = params.id;
+    });
+    this.plotObject = this.objectGraphMap.getSelectedObject();
+    const dropDownItems = this.objectGraphMap.listPlotTypes();
+    dropDownItems.forEach((val, idx) => {
+      this.plotTypeOptions.push({ id: idx.toString(), text: val });
     });
   }
 
