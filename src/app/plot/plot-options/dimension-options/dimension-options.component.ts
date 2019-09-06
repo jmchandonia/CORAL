ac7757753b8088e90e8a8ffda896edc907ce1a84
Charlie Trenholm <charleshtrenholm@gmail.com>
2019-09-06 16:35:51 -0700

    use reactive forms module with plotting and add values to object metadata model

--- src/app/plot/plot-options/dimension-options/dimension-options.component.ts
+++ src/app/plot/plot-options/dimension-options/dimension-options.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input, EventEmitter } from '@angular/core';
 import { Dimension } from '../../../shared/models/object-metadata';
 import { Select2OptionData } from 'ng2-select2';
 import { ObjectGraphMapService } from '../../../shared/services/object-graph-map.service';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-dimension-options',
@@ -19,21 +20,61 @@ export class DimensionOptionsComponent implements OnInit {
       this.fromDimensionDropdown.push({id: idx.toString(), text: val.type});
     });
   }
+  @Input() index: number;
+  @Input() set form(f: FormGroup) {
+    // tslint:disable-next-line:no-string-literal
+    this.dimensionForm = f.controls.dimensions['controls'][this.index];
+  }
   @Input() dimensionLabel = '';
   selectedValue: Dimension;
   allDimensions: Dimension[];
   fromDimensionDropdown: Array<Select2OptionData> = [{id: '', text: ''}];
+  dimensionForm: FormGroup;
+  displayValuesFrom: FormArray;
 
   constructor(
     private objectGraphMap: ObjectGraphMapService
   ) { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
   setSelectedDimension(event) {
     this.selectedValue = this.allDimensions[event.value];
-    this.objectGraphMap.setDimension(this.dimensionLabel, event.value);
+    this.dimensionForm.controls.fromDimension.setValue(event.data[0].text);
+    this.addDimensionVariables();
+  }
+
+  addDimensionVariables()  {
+    this.displayValuesFrom = this.dimensionForm.get('displayValuesFrom') as FormArray;
+
+    // clear form value on select
+    while (this.displayValuesFrom.length) {
+      this.displayValuesFrom.removeAt(0);
+    }
+
+    // add new values from selected dimension
+    if (this.selectedValue.dim_vars.length > 1) {
+      this.selectedValue.dim_vars.forEach(() => {
+        this.displayValuesFrom.push(new FormControl(false));
+      });
+    } else {
+      this.displayValuesFrom.push(new FormControl(true));
+    }
+  }
+
+  get displayValues() {
+    return this.displayValuesFrom;
+  }
+
+  get isDisplayAxisChecked() {
+    return this.dimensionForm.get('displayAxisLabels').value;
+  }
+
+  get isDisplayHoverChecked() {
+    return this.dimensionForm.get('displayHoverLabels').value;
+  }
+
+  updateDimensionVariable(event) {
   }
 
 }
--- src/app/plot/plot.module.ts
+++ src/app/plot/plot.module.ts
@@ -5,13 +5,14 @@ import { PlotRoutingModule } from './plot-routing.module';
 import { PlotOptionsComponent } from './plot-options/plot-options.component';
 import { Select2Module } from 'ng2-select2';
 import { DimensionOptionsComponent } from './plot-options/dimension-options/dimension-options.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [PlotComponent, PlotOptionsComponent, DimensionOptionsComponent],
   imports: [
     CommonModule,
     PlotRoutingModule,
     Select2Module,
+    ReactiveFormsModule
   ]
 })
 export class PlotModule { }
