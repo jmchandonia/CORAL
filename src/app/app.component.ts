8f3da22fa4ec367ac46f4a81bd3af42c3f887ae2
Charlie Trenholm <charleshtrenholm@gmail.com>
2019-09-24 17:58:00 -0700

    add functionality to /dashboard and /home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
@@ -6,18 +6,22 @@ import { Router, NavigationEnd } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'generix-ui';
   currentUrl: string;
   constructor(
     private router: Router
   ) {
+   }
+
+   ngOnInit() {
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
         this.currentUrl = event.url;
       }
     });
    }
+
 
 
 }
--- src/app/shared/components/home/home.component.ts
+++ src/app/shared/components/home/home.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { QueryParam, QueryMatch } from '../../models/QueryBuilder';
+import * as _ from 'lodash';
+import { HomeService } from '../../services/home.service';
+import { QueryBuilderService } from '../../services/query-builder.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +12,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  options: Select2Options = {
+    width: '100%'
+  };
+
+  private checkBoxArray: string[] = [];
+  public filterCategories: any[] = [];
+  private filterQueryBuilder: QueryParam[] = [];
+  private coreTypes: any[] = [];
+  private dynamicTypes: any[] = [];
+
+  constructor(
+    private homeService: HomeService,
+    private searchService: QueryBuilderService,
+    private router: Router
+    ) { }
 
   ngOnInit() {
+    this.getUpdatedValues();
+
+    this.homeService.getFilterValues()
+      .subscribe((res: any) => {
+        console.log('RES', res);
+        this.filterCategories = res.results;
+      });
+  }
+
+  getUpdatedValues() {
+    this.homeService.getUpdatedValues(this.filterQueryBuilder)
+    .subscribe((res: any) => {
+      console.log('RES', res);
+      if (res.status === 'OK') {
+        this.coreTypes = res.results.core_types;
+        this.dynamicTypes = res.results.dynamic_types;
+      }
+    });
+  }
+
+  onValueChecked(event) {
+
+    const [i, j] = event.target.id.split(' ').map(o => parseInt(o, 10));
+    const selected = this.filterCategories[i].items[j].queryParam as QueryParam;
+
+    if (this.checkBoxArray.includes(event.target.id)) {
+      this.filterQueryBuilder = this.filterQueryBuilder.filter(item => {
+        return !(_.isEqual(item, selected));
+      });
+      this.checkBoxArray = this.checkBoxArray.filter(item => item !== event.target.id);
+    } else {
+      this.checkBoxArray.push(event.target.id);
+      this.filterQueryBuilder.push(selected);
+    }
+    this.getUpdatedValues();
+  }
+
+  navigateToSearch(queryMatch: QueryMatch) {
+    queryMatch.params = this.filterQueryBuilder;
+    this.searchService.submitSearchResultsFromHome(queryMatch);
+    this.router.navigate(['/search/result']);
   }
 
 }
