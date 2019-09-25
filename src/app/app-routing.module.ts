bae7ca9d919b69ffa908af17728f4ec25f1cd415
Charlie Trenholm <charleshtrenholm@gmail.com>
2019-09-25 12:12:48 -0700

    UI: CSS improvements for dashboard, reports, and home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './shared/components/login/login.component';
 import { HomeComponent } from './shared/components/home/home.component';
 import { UploadComponent } from './shared/components/upload/upload.component'; // will be a module in the future
+import { ReportsComponent } from './shared/components/reports/reports.component';
 import { DashboardComponent } from './shared/components/dashboard/dashboard.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
   {path: 'home', component: HomeComponent},
-  {path: 'dashboard', component: DashboardComponent},
-  {path: 'upload', component: UploadComponent}
+  {path: 'reports', component: ReportsComponent},
+  {path: 'upload', component: UploadComponent},
+  {path: 'dashboard', component: DashboardComponent}
 ];
 
 @NgModule({
