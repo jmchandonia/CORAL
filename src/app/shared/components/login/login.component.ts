66ec179f0256e3ad1cb789c41f283e3dfe9162e4
Charlie Trenholm <charleshtrenholm@gmail.com>
2019-10-02 11:23:42 -0700

    Feature: Add loading circle and error display to login page

--- src/app/shared/components/login/login.component.ts
+++ src/app/shared/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../../shared/services/auth.service';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-login',
@@ -11,10 +12,12 @@ export class LoginComponent implements OnInit {
 
   constructor(
     private router: Router,
-    private auth: AuthService
+    private auth: AuthService,
+    private spinner: NgxSpinnerService
   ) { }
   public displayLogin = true;
   public error = false;
+  public loading = false;
   public errorMessage = '';
   username = '';
   password = '';
@@ -23,8 +26,12 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
+    this.loading = true;
+    this.spinner.show();
     this.auth.submitLogin(this.username, this.password)
       .subscribe(res => {
+        this.spinner.hide();
+        this.loading = false;
         if (res.success) {
           this.router.navigate(['home']);
         } else {
