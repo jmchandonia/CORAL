6ca28f5882932f0eca6b4af50d3bfdc7c2b1041d
Charlie Trenholm <charleshtrenholm@gmail.com>
2019-10-03 15:02:57 -0700

    Bug Fix: clear plot type after navigating to previous page to load dimensions

--- src/app/plot/plot-options/dimension-options/axis-labeler/axis-labeler.component.ts
+++ src/app/plot/plot-options/dimension-options/axis-labeler/axis-labeler.component.ts
@@ -65,7 +65,6 @@ export class AxisLabelerComponent implements OnInit, OnDestroy {
         }
       })
     ];
-    this.plotService.testLabelBuilders();
     this.plotService.updateFormatString(this.plotlyFormatString, this.axis);
   }
 
@@ -84,69 +83,5 @@ export class AxisLabelerComponent implements OnInit, OnDestroy {
   toggleDisplayOptions() {
     this.displayOptions = !this.displayOptions;
   }
-
-  // @Input() label: string;
-  // @Output() labelChanged: EventEmitter<string> = new EventEmitter();
-  // @Input() selected: DimensionRef;
-  // @Input() set values(v: DimensionRef) {
-  //   this.valueLabels = [];
-  //   this.format = '';
-  //   this._values = v;
-  //   if (this.values.dimVars.length === 1) {
-  //     this.valueLabels.push('');
-  //     this.format = '#V1';
-  //   } else {
-  //     const dv = this.values.dimVars;
-  //     dv.forEach((label, idx) => {
-  //       this.format += `${label.value}=#V${idx + 1}`;
-  //       if (idx !== dv.length - 1) {
-  //         this.format += ', ';
-  //       }
-  //     });
-  //     this.valueLabels = this.format.split(',').map(item => {
-  //       return item.replace(/#V[0-9]/gi, '');
-  //     });
-  //   }
-  //   this.labelChanged.emit(this.format);
-  // }
-
-  // get values() {
-  //   return this._values;
-  // }
-
-  // displayOptions = false;
-  // format = '';
-  // invalid = false;
-  // valueLabels = [];
-  // _values: DimensionRef;
-
-  // constructor() { }
-
-  // ngOnInit() { }
-
-  // toggleDisplayOptions() {
-  //   this.displayOptions = !this.displayOptions;
-  // }
-
-  // onSave() {
-  //   this.toggleDisplayOptions();
-  //   if (!this.displayOptions && this.format && this.format.match(/#V[0-9]/gi)) {
-  //     this.updateFormat();
-  //   } else {
-  //     // if format doesn't have #V(N),  reset value labels to original state
-  //     this.format = '';
-  //     this.valueLabels.forEach((value, idx) => {
-  //       this.format += `${value}#V${idx + 1},`;
-  //     });
-  //   }
-  // }
-
-  // updateFormat() {
-  //   // get value labels to display in UI
-  //   this.valueLabels = this.format.split(',').map(item => {
-  //     return item.replace(/#V[0-9]/gi, '');
-  //   });
-  //   this.labelChanged.emit(this.format);
-  // }
 
 }
