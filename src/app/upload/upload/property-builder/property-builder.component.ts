ce7aedc4534bb069d2c234a944b3e3782e73ac0f
Charlie Trenholm <charleshtrenholm@gmail.com>
2019-10-04 14:04:04 -0700

    Feature: create navigation breadcrumb UI for upload wizard

---
+++ src/app/upload/upload/property-builder/property-builder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-property-builder',
+  templateUrl: './property-builder.component.html',
+  styleUrls: ['./property-builder.component.css']
+})
+export class PropertyBuilderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
