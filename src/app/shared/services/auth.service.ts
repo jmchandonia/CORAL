e81934cf0c33c133c4c53f3802ba667e218459a7
Charlie Trenholm <charleshtrenholm@gmail.com>
2019-10-14 09:51:51 -0700

    clear unnecessary service methods

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +16,6 @@ export class AuthService {
 
 
   isAuthenticated() {
-    return true;
     const token = localStorage.getItem('authToken');
     if (!token) {
       return false;
@@ -25,7 +25,7 @@ export class AuthService {
   }
 
   submitLogin(username, password) {
-    return this.http.post<any>('https://psnov1.lbl.gov:8082/generix/user_login', {username, password})
+     return this.http.post<any>(`${environment.baseURL}/user_login`, {username, password})
       .pipe(map(res => {
         const success = res.success;
         if (success) {
--- src/app/shared/services/query-builder.service.ts
+++ src/app/shared/services/query-builder.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { QueryBuilder, QueryMatch, QueryParam } from '../models/QueryBuilder';
 import { Subject } from 'rxjs';
-import { NetworkService } from './network.service';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
@@ -12,7 +11,6 @@ export class QueryBuilderService {
   public queryBuilderObject: QueryBuilder = new QueryBuilder();
   public queryBuilderSub = new Subject<QueryBuilder>();
   resultSub = new Subject();
-  public resultStore: any;
   searchType: string;
 
   constructor(private http: HttpClient) { }
@@ -24,66 +22,6 @@ export class QueryBuilderService {
   resetObject() {
     this.queryBuilderObject = new QueryBuilder();
   }
-
-  getUpdatedObject() {
-    // refactor this
-    return this.queryBuilderSub.asObservable();
-  }
-
-
-  // getCurrentObject() {
-  //   if (!this.queryBuilderObject) {
-  //     const savedObject = this.getSavedObject();
-  //     if (savedObject) {
-  //       this.queryBuilderObject = savedObject;
-  //       return {qb: this.queryBuilderObject, empty: false};
-  //     } else {
-  //       this.queryBuilderObject = new QueryBuilder();
-  //       return {qb: this.queryBuilderObject, empty: true};
-  //     }
-  //   } else {
-  //     return {qb: this.queryBuilderObject, empty: false};
-  //   }
-  // }
-
-  // getSavedObject() {
-  //   return JSON.parse(localStorage.getItem('queryBuilderObject'));
-  // }
-
-  // resetObject() {
-  //   localStorage.removeItem('queryBuilderObject');
-  //   this.queryBuilderObject = new QueryBuilder();
-  // }
-
-  // getUpdatedObject() {
-  //   return this.queryBuilderSub.asObservable();
-  // }
-
-  // updateQueryMatch(connection, queryMatch) {
-  //   this.queryBuilderObject[connection] = queryMatch;
-  //   this.queryBuilderSub.next(this.queryBuilderObject);
-  // }
-
-  // addProcessParam(process, queryParam) {
-  //   this.queryBuilderObject[process].push(queryParam);
-  //   this.queryBuilderSub.next(this.queryBuilderObject);
-  // }
-
-  // updateProcessParam(process, index, event) {
-  //   this.queryBuilderObject[process][index][event.key] = event.value.value;
-  //   this.queryBuilderSub.next(this.queryBuilderObject);
-  // }
-
-  // removeProcessParam(process, queryParam) {
-  //   this.queryBuilderObject[process] = this.queryBuilderObject[process]
-  //     .filter(param => param !== queryParam);
-  //   this.queryBuilderSub.next(this.queryBuilderObject);
-  // }
-
-  // submitSearchResults() {
-  //   console.log('QBO IN SUBMIT', this.queryBuilderObject);
-  //   localStorage.setItem('queryBuilderObject', JSON.stringify(this.queryBuilderObject));
-  // }
 
   submitSearchResultsFromHome(queryMatch: QueryMatch) {
     this.queryBuilderObject = new QueryBuilder();
