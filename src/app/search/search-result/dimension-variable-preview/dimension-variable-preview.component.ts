ab63ff42a512e5bbe6c4a973e8c782a04836441b
Charlie Trenholm <charleshtrenholm@gmail.com>
2019-11-18 14:32:33 -0800

    UI: make dimension variable preview modal table a DataTable

--- src/app/search/search-result/dimension-variable-preview/dimension-variable-preview.component.ts
+++ src/app/search/search-result/dimension-variable-preview/dimension-variable-preview.component.ts
@@ -1,34 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { QueryBuilderService } from 'src/app/shared/services/query-builder.service';
 import { NgxSpinnerService } from 'ngx-spinner';
-
+import 'datatables.net';
+import 'datatables.net-bs4';
+import * as $ from 'jquery';
 @Component({
   selector: 'app-dimension-variable-preview',
   templateUrl: './dimension-variable-preview.component.html',
   styleUrls: ['./dimension-variable-preview.component.css']
 })
-export class DimensionVariablePreviewComponent implements OnInit {
+export class DimensionVariablePreviewComponent implements OnInit, AfterViewInit {
 
   constructor(
-    private queryBuilder: QueryBuilderService,
-    private spinner: NgxSpinnerService
   ) { }
 
   id: string;
   index: number;
   numberOfRows: number;
   data: any;
+  dataResults: any[] = [];
+  dataTable: any;
+
+  @ViewChild('tableModal') el: ElementRef;
 
   ngOnInit() {
-    this.spinner.show();
-    this.queryBuilder.getDimensionVariableValues(this.id, this.index)
-      .subscribe((data: any) => {
-        this.spinner.hide();
-        this.data = data.results;
-        this.numberOfRows = this.data.size > this.data.max_row_count
-          ? this.data.max_row_count
-          : this.data.size;
-      });
+    this.dataResults = this.data.dim_vars;
+    this.numberOfRows = this.data.size > this.data.max_row_count
+      ? this.data.max_row_count
+      : this.data.size;
+  }
+
+  ngAfterViewInit() {
+    if (this.el) {
+            const table: any = $(this.el.nativeElement);
+            this.dataTable = table.DataTable();
+          }
   }
 
 }
