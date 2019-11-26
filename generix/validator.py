a168a11fd541ff043b296e3433041592c9587a69
Pavel Novichkov <pnovichkov@gmail.com>
2019-11-25 19:53:37 -0800

    [new class] ValueValidationService

--- generix/validator.py
+++ generix/validator.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import re
 from .ontology import Term, OntologyService
 from . import services
@@ -93,3 +94,104 @@ class TermValidatorService:
                 self.__write_error('value term not found', term)
 
         self.__f.write('\n')
+
+
+class ValueValidationService:
+
+    def cast_var_values(self, values, var_term_id):
+        var_term = services.ontology._get_term(var_term_id)
+        scalar_type = var_term.scalar_type
+        errors = []
+        if scalar_type == 'int':
+            errors = self.cast_values(values, int, 'int')
+        elif scalar_type == 'float':
+            errors = self.cast_values(values, float, 'float')
+        elif scalar_type == 'string':
+            errors = self.cast_values(values, str, 'string')
+        elif scalar_type == 'oterm':
+            errors = self.cast_oterm_values(values, var_term)
+
+        # TODO: cast object refs
+        return errors
+
+    def __validate_values_type(self, values):
+        if type(values) != np.ndarray:
+            raise ValueError('Wrond type: %s'% str(type(values)))
+        if values.dtype != 'object':
+            raise ValueError('Type of array should be object, but it is %s'% str(values.dtype))    
+
+
+    def cast_values(self, values, cast_function, cast_type):
+
+        self.__validate_values_type(values)
+
+        errors = []
+        with np.nditer(values, op_flags=['readwrite'], flags=['multi_index', 'refs_ok']) as it:
+            while not it.finished:                 
+                casted_value = None
+                
+                value = it[0].item()
+                if value:
+                    try: 
+                        casted_value = cast_function(value)
+                    except: 
+                        self.__add_error(errors, it.multi_index, value, cast_type)
+
+                it[0] = casted_value
+                it.iternext()
+
+        return errors 
+
+    def cast_oterm_values(self, values, var_term):
+        
+        self.__validate_values_type(values)
+
+        # Get a unique set of terms 
+        terms = {}
+        for val in np.nditer(values, flags=['refs_ok']):
+            val = val.item()
+            if val is not None:
+                terms[val] = {
+                    'term': None,
+                    'error': None
+                }
+
+        # Get terms
+        ont = services.ontology
+        for term_id_name in terms.keys():
+            try:
+                term = ont._get_term(term_id_name)
+                if var_term.term_id in term.parent_path_ids:
+                    terms[term_id_name]['term'] = term                
+                else:
+                    terms[term_id_name]['error'] = 'Term %s does not have a valid parent %s' % (str(term), str(var_term) )    
+            except:
+                terms[term_id_name]['error'] = 'Can not find term: %s' % term_id_name
+
+        # cast values
+        errors = []
+        with np.nditer(values, op_flags=['readwrite'], flags=['multi_index', 'refs_ok']) as it:
+            while not it.finished:                 
+                casted_value = None
+                
+                value = it[0].item()
+                if value:
+                    term_record = terms[value]
+                    casted_value = term_record['term']
+                    error = term_record['error']
+                    if casted_value is None and error is not None:
+                        self.__add_error(errors, it.multi_index, value, 'Term', error)
+
+                if casted_value is not None:
+                    casted_value = str(casted_value)
+                it[0] = casted_value
+                it.iternext()
+        return errors 
+
+
+    def __add_error(self, errors, index, value, cast_type, err_msg = ''):
+        errors.append({
+            'index': index[0] if len(index) == 1 else index,
+            'error_msg': 'Can not cast "%s" to type %s. %s' % (value, cast_type, err_msg)
+        })
+    
