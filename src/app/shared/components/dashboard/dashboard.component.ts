fba7304771d2c7a33bb60d84576695d15f9c7370
Charlie Trenholm <charleshtrenholm@gmail.com>
2019-11-27 14:36:33 -0800

    UI: change dashboard sample plots to live plotly instances

--- src/app/shared/components/dashboard/dashboard.component.ts
+++ src/app/shared/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PlotService } from '../../services/plot.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    // private plotService: PlotService
+  ) { }
 
   ngOnInit() {
   }
