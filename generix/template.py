44b8bd104bba204ee6b874b7a8e796883a5394ac
Pavel Novichkov <pnovichkov@gmail.com>
2019-12-02 22:35:45 -0800

    [new method _get_contexted_var_name

--- generix/template.py
+++ generix/template.py
@@ -3,76 +3,9 @@ import numpy as np
 from openpyxl import Workbook, load_workbook
 from openpyxl.styles import PatternFill, Font
 from openpyxl.utils import get_column_letter
+ 
 
-def generate_brick_2d_template_draft(brick_ds,file_name):
-    book = Workbook()
-    sheet = book.active
-
-    dims = ['Gene', 'Condition']
-
-    dim1_vars = ['Gene:orgId', 'Gene:locusId']
-    dim1_row_counts = 10
-
-    dim2_vars = ['Condition:name', 'Condition:time']
-    dim2_row_counts = 10
-
-    data_type ='TNSeq<Gene, Condition>'
-
-    rows = [
-        ['Automatically generated template '],
-        ['Data type', 'TNSeq<Gene, Condition>'],
-        [],
-        ['Instructions: fill in all colored parts of the spreadheet with your data and metadata'],
-        ['Gene metadata'],
-        ['Condition metadata'],
-        ['Data'],    
-        [],
-        [],
-        ['Format: F2D', '', 'Condition:name'],
-        ['Gene:orgId', 'Gene:locusId', 'Condition:time']
-    ]
-
-    for row in rows:
-        sheet.append(row)
-
-    dim1_fill = PatternFill('solid', fgColor="FFFFDD")  
-    dim2_fill = PatternFill('solid', fgColor="DDFFDD")
-    data_fill = PatternFill('solid', fgColor="FFDDDD")  
-        
-    sheet['A1'].font = Font(name='Calibri', size=14)
-    sheet['A4'].font = Font(name='Calibri', size=12, bold=True, italic=True)
-    sheet['A5'].fill = dim1_fill
-    sheet['A6'].fill = dim2_fill 
-    sheet['A7'].fill = data_fill
-    sheet['A10'].font = Font(name='Calibri', size=12, bold=True)
-
-    start_row = 12        
-    start_column = 1
-    for ri in range(start_row,start_row + dim1_row_counts):
-        for ci,_ in enumerate(dim1_vars):
-            sheet.cell(row=ri, column=ci+start_column).fill = dim1_fill 
-
-    start_row = 10        
-    start_column = len(dim2_vars) + 2
-    for ri,_ in enumerate(dim2_vars):
-        for ci in range(start_column,start_column + dim2_row_counts):
-            sheet.cell(row=ri+ start_row, column=ci ).fill = dim2_fill 
-        
-    start_row = 12      
-    start_column = len(dim2_vars) + 2    
-    for ri in range(start_row,start_row + dim1_row_counts):
-        for ci in range(start_column,start_column + dim2_row_counts):
-            sheet.cell(row=ri, column=ci ).fill = data_fill 
-        
-        
-    for i in range(1, len(dim2_vars) + 2 ):
-        c = get_column_letter(i)
-        sheet.column_dimensions[c].width = 18    
-
-    book.save(file_name)    
-
-
-def generate_brick_2d_template(brick_ds,file_name):
+def generate_brick_F2D_template(brick_ds,file_name):
 
     TITLE_FONT = Font(name='Calibri', size=14) 
     INSTRUCTIONS_FONT = Font(name='Calibri', size=10, bold=True, italic=True)
@@ -94,16 +27,18 @@ def generate_brick_2d_template(brick_ds,file_name):
 
     template_name = brick_ds['template_type']
     data_type = brick_ds['type']['text']
-    data_var = data_vars[0]['type']['text']
+    data_var = _get_contexted_var_name(data_vars[0])
 
     dims = [brick_dims[0]['type']['text'], brick_dims[1]['type']['text']]
     dim1_vars = []
     for dim_var in brick_dims[0]['variables']:
-        dim1_vars.append( '%s:%s' % (dims[0], dim_var['type']['text'])  )
+        var_name = _get_contexted_var_name(dim_var)
+        dim1_vars.append( '%s:%s' % (dims[0], var_name)  )
 
     dim2_vars = []
     for dim_var in brick_dims[1]['variables']:
-        dim2_vars.append('%s:%s' % (dims[1], dim_var['type']['text']) )
+        var_name = _get_contexted_var_name(dim_var)
+        dim2_vars.append('%s:%s' % (dims[1], var_name) )
 
     rows = [
         ['Automatically generated template '],
@@ -177,7 +112,7 @@ def generate_brick_2d_template(brick_ds,file_name):
 
     book.save(file_name)    
 
-def generate_brick_1dm_template(brick_ds,file_name):
+def generate_brick_F1DM_template(brick_ds,file_name):
 
     TITLE_FONT = Font(name='Calibri', size=14) 
     INSTRUCTIONS_FONT = Font(name='Calibri', size=10, bold=True, italic=True)
@@ -197,12 +132,13 @@ def generate_brick_1dm_template(brick_ds,file_name):
 
     template_name = brick_ds['template_type']
     data_type = brick_ds['type']['text']
-    data_vars = [ dvar['type']['text'] for dvar in data_vars ] 
+    data_vars = [ _get_contexted_var_name(dvar) for dvar in data_vars ] 
 
     dims = [brick_dims[0]['type']['text']]
     dim1_vars = []
     for dim_var in brick_dims[0]['variables']:
-        dim1_vars.append( '%s:%s' % (dims[0], dim_var['type']['text'])  )
+        var_name = _get_contexted_var_name(dim_var)
+        dim1_vars.append( '%s:%s' % (dims[0], var_name)  )
 
     rows = [
         ['Automatically generated template '],
@@ -268,11 +204,12 @@ def parse_brick_F1DM_data(brick_ui, file_name):
 
     dims_ui = brick_ui['dimensions']
 
-    template_data_vars = [ dvar['type']['text'] for dvar in brick_ui['dataValues'] ] 
+    template_data_vars = [ _get_contexted_var_name(dvar) for dvar in brick_ui['dataValues'] ] 
     template_dim = dims_ui[0]['type']['text']
     template_dim1_vars = []
     for dim_var in dims_ui[0]['variables']:
-        template_dim1_vars.append( '%s:%s' % (template_dim, dim_var['type']['text'])  )
+        var_name = _get_contexted_var_name(dim_var)
+        template_dim1_vars.append( '%s:%s' % (template_dim, var_name)  )
 
     dims = [{
         'size' : 0,
@@ -328,8 +265,18 @@ def parse_brick_F1DM_data(brick_ui, file_name):
         'data_vars': data_vars
     }
 
+def parse_brick_F2D_data(brick_ui, file_name):
+    dims = []
+    data_vars = []
 
-def parse_brick_F2DT_data(brick_ui, file_name):
+    _validate(dims, data_vars)
+    return {
+        'dims': dims,
+        'data_vars': data_vars
+    }
+
+
+def parse_brick_F2D_CSV_data(brick_ui, file_name):
     # Read df
     df = pd.read_csv(file_name, sep='\t') 
 
@@ -413,3 +360,18 @@ def _get_1d_column_data(sheet, ri, ci):
         data.append(value)
         ri += 1
     return data
+
+def _get_contexted_var_name(dvar):
+    var_name = dvar['type']['text']
+    if 'units' in dvar and dvar['units']['text']:
+        var_name += ' (%s)' % dvar['units']['text']
+
+    items = []
+    if 'context' in dvar:
+        for ce in dvar['context']:
+            item = '%s=%s' % (ce['type']['text'], ce['value']['text'])
+            if 'units' in ce and ce['units']['text']:
+                item += ' (%s)' % ce['units']['text']
+            items.append(item)
+
+    return var_name if len(items) == 0 else '%s:%s' % (var_name, '; '.join(items))
