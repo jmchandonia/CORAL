8fa98e78a2857dc78a8f2a405cafab1caa2417a9
Charlie Trenholm <charleshtrenholm@gmail.com>
2020-05-11 10:15:25 -0700

    testing: write tests for advanced search and query builder components

--- src/app/search/advanced-search/query-builder/query-builder.component.spec.ts
+++ src/app/search/advanced-search/query-builder/query-builder.component.spec.ts
@@ -1,13 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { Spectator, createComponentFactory } from '@ngneat/spectator';
+import { Spectator, createComponentFactory, mockProvider } from '@ngneat/spectator';
 import { QueryBuilderComponent } from './query-builder.component';
 import { Select2Module } from 'ng2-select2';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { MockComponent } from 'ng-mocks';
 import { PropertyParamsComponent } from './property-params/property-params.component';
+import { QueryBuilderService } from 'src/app/shared/services/query-builder.service';
+import { Subject } from 'rxjs';
 
-describe('QueryBuilderComponent', () => {
+fdescribe('QueryBuilderComponent', () => {
+
+  const MockQueryBuilder = {
+    getDataTypes: () => new Subject(),
+    getLoadedDataTypes: () => [{test: 'test'}]
+  };
+
   let spectator: Spectator<QueryBuilderComponent>;
   const createComponent = createComponentFactory({
     component: QueryBuilderComponent,
@@ -18,12 +26,21 @@ describe('QueryBuilderComponent', () => {
     ],
     entryComponents: [
       MockComponent(PropertyParamsComponent)
-    ]
+    ],
+    providers: [
+      mockProvider(QueryBuilderService, MockQueryBuilder)
+    ],
   });
 
   beforeEach(() => spectator = createComponent());
 
   it('should create', () => {
     expect(spectator.component).toBeTruthy();
+  });
+
+  it('should load data types', () => {
+    const spy = spyOn(spectator.component, 'populateDataTypes').and.callThrough();
+    expect(spy);
+    expect(spectator.component.dataTypes).toEqual([{test: 'test'}]);
   });
 });
