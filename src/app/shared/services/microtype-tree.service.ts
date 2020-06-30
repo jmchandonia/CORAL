26ee263fe08a4f0174250727124e90476b0bfc45
Charles Trenholm <charleshtrenholm@gmail.com>
2020-06-30 15:15:48 -0700

    Feature: create models and services for sorted microtype tree object

---
+++ src/app/shared/services/microtype-tree.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { MicrotypeTreeFactoryService as MicrotypeTreeFactory } from './microtype-tree-factory.service';
+import { HttpClient } from '@angular/common/http';
+import { MicroTypeTreeNode } from '../models/microtype-tree';
+const mockMicrotypes = require('src/app/shared/test/mock_microtypes.json');
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MicrotypeTreeService {
+
+  constructor(private http: HttpClient) {
+    // this.getMicrotypes();
+  }
+
+  getMicrotypes(): Promise<MicroTypeTreeNode[]> {
+    return new Promise((resolve, reject) => {
+      resolve(MicrotypeTreeFactory.createMicrotypeTree(mockMicrotypes))
+    });
+  }
+}
