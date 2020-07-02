a999c07e509ff78ce7d0cedc29502723dc97ee4b
Charles Trenholm <charleshtrenholm@gmail.com>
2020-07-01 17:07:38 -0700

    UI: filter non microtypes without children and add badge labels for types

--- src/app/shared/services/microtype-tree-factory.service.ts
+++ src/app/shared/services/microtype-tree-factory.service.ts
@@ -16,10 +16,11 @@ export class MicrotypeTreeFactoryService {
     let microtypeTree: Array<MicroTypeTreeNode> = [];
     microtypes.results.forEach((microtype: MicroTypeTreeNode) => {
         if(microtype.mt_parent_term_ids.length) {
-            microtype.mt_parent_term_ids.forEach((id: string) => {
+            microtype.mt_parent_term_ids
+              .forEach((id: string) => {
                 // there are some items with parents that are not in the results
                 if(hashTable[id] !== undefined) {
-                    hashTable[id].children.push(hashTable[microtype.term_id]);
+                    hashTable[id].children.push({...hashTable[microtype.term_id]});
                 }
             });
         } else {
@@ -27,5 +28,10 @@ export class MicrotypeTreeFactoryService {
         }
     });
     return microtypeTree;
+  }
+
+  filterNonMicrotypes(microtypes: MicroTypeTreeNode[]) {
+    // filter out non-microtypes that don't have children
+
   }
 }
