0b44db5f2c6b616c7556940dfe4282b23a74aaf2
Charles Trenholm <charleshtrenholm@gmail.com>
2020-07-01 21:04:25 -0700

    Feat: add initial prototype keyword search implementation to microtype tree

--- src/app/shared/components/microtype-browser/microtype-browser.component.ts
+++ src/app/shared/components/microtype-browser/microtype-browser.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, ViewChild, ElementRef } from '@angular/core';
 import { UploadService } from '../../services/upload.service';
 import * as $ from 'jquery';
 import 'datatables.net-bs4';
@@ -6,7 +6,7 @@ import 'datatables.net';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { MicrotypeTreeService } from 'src/app/shared/services/microtype-tree.service';
 import { MicroTypeTreeNode } from 'src/app/shared/models/microtype-tree';
-import { ITreeOptions } from 'angular-tree-component'
+import { ITreeOptions, TreeComponent, TreeModel } from 'angular-tree-component'
 
 @Component({
   selector: 'app-microtype-browser',
@@ -18,6 +18,7 @@ export class MicrotypeBrowserComponent implements OnInit {
   public microtypes: MicroTypeTreeNode[];
   dataTables: any;
   public treeOptions: ITreeOptions = { displayField: 'term_def' }
+  @ViewChild('tree', {static: false}) tree: TreeComponent;
 
   constructor(
     private uploadService: UploadService,
@@ -31,12 +32,15 @@ export class MicrotypeBrowserComponent implements OnInit {
       .then((microtypes: MicroTypeTreeNode[]) => {
         this.microtypes = microtypes;
         this.chRef.detectChanges();
-        console.log('MICROTYPES', this.microtypes);
       });
   }
 
   getMicrotypeMetadata(event) {
     console.log('getting microtype metadata', event.node.data);
+  }
+
+  setKeywordSearchFilter(event) {
+    this.tree.treeModel.filterNodes(event.target.value);
   }
 
 }
