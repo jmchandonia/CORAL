4a3ccec08a78b1700e8de42a57a16359e178d8e8
Charles Trenholm <charleshtrenholm@gmail.com>
2020-07-15 12:07:50 -0700

    Feat: add http call to get microtypes from backend

--- src/app/shared/services/microtype-tree.service.ts
+++ src/app/shared/services/microtype-tree.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { MicrotypeTreeFactoryService as MicrotypeTreeFactory } from './microtype-tree-factory.service';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 import { MicroTypeTreeNode } from '../models/microtype-tree';
-// const mockMicrotypes = require('src/app/shared/test/mock_microtypes.json');
 const mockMicrotypes = require('src/app/shared/test/mock_microtypes_3.json');
 
 @Injectable({
@@ -18,8 +18,12 @@ export class MicrotypeTreeService {
 
   getMicrotypes(): Promise<MicroTypeTreeNode[]> {
     return new Promise((resolve, reject) => {
-      this.microtypes = MicrotypeTreeFactory.createMicrotypeTree(mockMicrotypes.results);
-      resolve(this.microtypes);
+      // this.microtypes = MicrotypeTreeFactory.createMicrotypeTree(mockMicrotypes.results);
+      // resolve(this.microtypes);
+      this.http.get(`${environment.baseURL}/microtypes`).subscribe((data: any) => {
+        this.microtypes = MicrotypeTreeFactory.createMicrotypeTree(data.results);
+        resolve(this.microtypes);
+      })
     });
   }
 }
