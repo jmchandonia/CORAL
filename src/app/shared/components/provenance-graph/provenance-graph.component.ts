8c5607265c8b05e796bab2c16a529d3e9fa07995
Charles Trenholm <charleshtrenholm@gmail.com>
2020-07-17 07:09:58 -0700

    Feat: add initial UI and packages for VisJS

--- src/app/shared/components/provenance-graph/provenance-graph.component.ts
+++ src/app/shared/components/provenance-graph/provenance-graph.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
-const prov = require('src/app/shared/test/mock-provenance-graph.json').result;
-import * as d3 from 'd3';
+// import * as d3 from 'd3';
+// import * as vis from 'vis-network';
+import { Network, DataSet } from 'vis';
+const mockData = require('src/app/shared/test/mock-provenance-graph.json');
 
 @Component({
   selector: 'app-provenance-graph',
@@ -9,77 +11,52 @@ import * as d3 from 'd3';
 })
 export class ProvenanceGraphComponent implements OnInit, AfterViewInit {
 
-  margin = {top: 1, right: 1, bottom: 6, left: 1};
+  nodes: { id: number, label: string, data: any }[];
+  edges: { from: number, to: number }[];
 
-  width = 600;
-  height = 900;
+  @ViewChild('pGraph') pGraph: ElementRef;
 
-  @ViewChild('provenanceGraph') pGraph: ElementRef;
-  svg: any;
-  // svg: d3.Selection<SVGElement>;
-  path: any;
+  options = { physics: false };
 
-  constructor() { }
+  network: any; // TODO: see if there is @types/vis-network
+
+  constructor(
+  ) { }
 
   ngOnInit(): void {
   }
 
-  ngAfterViewInit() {
-        const simulation = d3.forceSimulation(prov.nodes)
-          .force('charge', d3.forceManyBody().strength(-100))
-          .force('center', d3.forceCenter(this.width / 2, this.height / 2))
-          .force('link', d3.forceLink().links(prov.links))
-          .on('tick', ticked);
+  ngAfterViewInit(): void {
+    this.nodes = new DataSet(
+      mockData.result.nodes.map(node => {
+        // {id: node.index, label: `${node.count} ${node.name}`}
+        return {
+          id: node.index,
+          label: `${node.count} ${node.name}`,
+          // color: node.type === 'dynamic' ? 'blue' : 'red',
+          color: {
+            background: 'white',
+            border: node.type === 'dynamic' ? 'rgb(246, 139, 98)' : 'rgb(78, 111, 182)',
+            hover: { background: '#ddd' }
+          },
+          // opacity: node.type === 'null' ? 0 : 1 
+          shape: 'box'
+        }
+      })
+    );
 
-          function ticked() {
-            updateLinks();
-            updateNodes();
-          }
+    this.edges = new DataSet(
+      mockData.result.links.map(edge => ({from: edge.source, to: edge.target}))
+    );
 
-          function updateLinks() {
-            var u: any = d3.select('.links')
-              .selectAll('line')
-              .data(prov.links);
+    this.network = new Network(
+      this.pGraph.nativeElement,
+      {
+        nodes: this.nodes,
+        edges: this.edges
+      },
+      this.options
+    );
+ }
 
-            u.enter()
-              .append('line')
-              .merge(u)
-              .attr('x1', function(d) { return d.source.x })
-              .attr('y1', function(d) { return d.source.y })
-              .attr('x2', function(d) { return d.target.x })
-              .attr('y2', function(d) { return d.target.y })
-              .attr('stroke', 'black');
-
-            u.exit().remove();
-          }
-
-          function updateNodes() {
-            const v: any = d3.select('.nodes')
-              .selectAll('text')
-              .data(prov.nodes)
-
-            v.enter()
-              // .append('circle')
-              // .attr('r', 50)
-              // .attr('cx', function(d) { return d.x })
-              // .attr('cy', function(d) { return d.y })
-              // .attr('fill', "#CED4DA")
-              .append('text')
-              .text(function(d) { return d.count + ' ' + d.name })
-              .merge(v)
-              .attr('x', function(d) { return d.x })
-              .attr('y', function(d) { return d.y })
-              .attr('dy', function(d) { return 5 })
-
-              v.exit().remove();
-          }
-
-        // simulation.force('link',
-        //   d3.forceLink()
-        //     .distance(5)
-        //     .strength(2)
-        // );
-
-      // });
-  }
 }
