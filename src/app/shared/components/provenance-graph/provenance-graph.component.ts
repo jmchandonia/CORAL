d0c38d691e6cb0d29d0fc3dd8a5c348531854926
Charles Trenholm <charleshtrenholm@gmail.com>
2020-07-28 20:09:09 -0700

    Feat: add option for expandable and collapsible nodes

--- src/app/shared/components/provenance-graph/provenance-graph.component.ts
+++ src/app/shared/components/provenance-graph/provenance-graph.component.ts
@@ -11,7 +11,7 @@ import { QueryMatch } from 'src/app/shared/models/QueryBuilder';
 export class ProvenanceGraphComponent implements OnInit, AfterViewInit {
 
   nodes:  DataSet;
-  edges: { from: number, to: number }[];
+  edges: DataSet;
   @Output() querySelected: EventEmitter<QueryMatch> = new EventEmitter();
 
   @ViewChild('pGraph') pGraph: ElementRef;
@@ -22,13 +22,6 @@ export class ProvenanceGraphComponent implements OnInit, AfterViewInit {
       dragView: false,
       hover: true
     },
-    physics: {
-      // enabled: false,
-      hierarchicalRepulsion: {
-        // avoidOverlap: 1
-        // nodeDistance: 200
-      }
-    },
     layout: {
       hierarchical: {
         direction: 'UD',
@@ -38,6 +31,11 @@ export class ProvenanceGraphComponent implements OnInit, AfterViewInit {
       }
     }
   };
+
+  // clusters parent nodes together with expandable children
+  clusterOptions = {
+    joinCondition: (nodeOptions) => nodeOptions.cid == 2
+  }
 
   network: any; // TODO: see if there is @types/vis-network
 
@@ -72,60 +70,118 @@ export class ProvenanceGraphComponent implements OnInit, AfterViewInit {
     });
 
     // add double click event listener to submit search query on nodes
-    this.network.on('doubleClick', (({nodes}) => {
+    this.network.on('doubleClick', ({nodes}) => this.submitSearchQuery(nodes));
+
+    this.network.on('click', ({nodes}) => this.expandNodes(nodes));
+
+    // this.network.clustering.cluster(this.clusterOptions);
+ }
+
+  createNode(dataItem: any) {
+    const node: any = {
+      id: dataItem.index,
+      label: dataItem.category !== 'null' ? `${dataItem.count} ${dataItem.name}` : '',
+      font: {
+        size: 16
+      },
+      color: {
+        background: dataItem.category !== 'null' ? 'white' : 'rgba(0,0,0,0)',
+        border: dataItem.category === 'DDT_' ? 'rgb(246, 139, 98)' : 'rgb(78, 111, 182)',
+        hover: { background: '#ddd' }
+      },
+      borderWidth: dataItem.category === 'null' ? 0 : 1,
+      physics: false,
+      shape: 'box',
+      data: {...dataItem}
+    }
+
+    if (dataItem.children) {
+      node.data.childrenExpanded = false;
+    }
+
+    return node;
+  }
+
+  createEdge(edgeItem: any) {
+    const target = this.nodes.get(edgeItem.target);
+    return {
+      from: edgeItem.source,
+      to: edgeItem.target,
+      label: 'label',
+      font: {
+        size: 16,
+        color: 'rgba(0,0,0,0)',
+        strokeColor: 'rgba(0,0,0,0)'
+      },
+      arrows: {
+        to: {
+          enabled: target.data.category !== 'null'
+        }
+      },
+      chosen: {
+        edge: (values, id, selected, hovering) => {
+          values.width = 2;
+        },
+        label: (values, id, selected, hovering) => {
+          // make label visible on edge click
+          values.color = 'black';
+          values.strokeColor = 'white';
+        },
+      }
+    }
+ }
+
+  submitSearchQuery(nodes) {
+    if (nodes.length) {
       const { category, dataType, dataModel } = this.nodes.get(nodes)[0].data;
       const query = new QueryMatch({category, dataType, dataModel});
       this.querySelected.emit(query);
-    }));
- }
-
- createNode(dataItem: any) {
-  const node: any = {
-    id: dataItem.index,
-    label: dataItem.category !== 'null' ? `${dataItem.count} ${dataItem.name}` : '',
-    font: {
-      size: 16
-    },
-    color: {
-      background: dataItem.category !== 'null' ? 'white' : 'rgba(0,0,0,0)',
-      border: dataItem.category === 'DDT_' ? 'rgb(246, 139, 98)' : 'rgb(78, 111, 182)',
-      hover: { background: '#ddd' }
-    },
-    borderWidth: dataItem.category === 'null' ? 0 : 1,
-    physics: false,
-    shape: 'box',
-    data: {...dataItem}
-  }
-  return node;
- }
-
- createEdge(edgeItem: any) {
-   const target = this.nodes.get(edgeItem.target);
-   return {
-     from: edgeItem.source,
-     to: edgeItem.target,
-     label: 'label',
-     font: {
-       size: 16,
-       color: 'rgba(0,0,0,0)',
-       strokeColor: 'rgba(0,0,0,0)'
-     },
-     arrows: {
-       to: {
-         enabled: target.data.category !== 'null'
-       }
-     },
-     chosen: {
-      edge: (values, id, selected, hovering) => {
-        values.width = 2;
-      },
-      label: (values, id, selected, hovering) => {
-        // make label visible on edge click
-        values.color = 'black';
-        values.strokeColor = 'white';
-      },
     }
-   }
- }
+  }
+
+  expandNodes(nodes) {
+    if (nodes.length) {
+      const node = this.nodes.get(nodes)[0]
+      if (node.data.children) {
+        if (!node.data.childrenExpanded) {
+          node.data.children.forEach(child => {
+            this.nodes.update(this.createNode(child));
+            // TODO: update createEdge method to handle both cases so there arent repeated methods
+            this.edges.update({
+              from: node.id,
+              to: child.index,
+              label: 'label',
+              font: {
+                size: 16,
+                color: 'rgba(0,0,0,0)',
+                strokeColor: 'rgba(0,0,0,0)'
+              },
+              arrows: {
+                to: {
+                  enabled: child.category !== 'null'
+                }
+              },
+              chosen: {
+                edge: (values, id, selected, hovering) => {
+                  values.width = 2;
+                },
+                label: (values, id, selected, hovering) => {
+                  // make label visible on edge click
+                  values.color = 'black';
+                  values.strokeColor = 'white';
+                },
+              }
+            });
+          });
+          node.data.childrenExpanded = true;
+          this.network.fit({animation: true})
+        } else {
+          this.nodes.remove(node.data.children.map(child => child.index));
+          node.data.childrenExpanded = false;
+          this.network.fit({animation: true});
+        }
+      }
+    }
+  }
 
 }
