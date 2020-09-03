ac897c5278b50da5b88767ba67e4cf1ded10b2d3
Charles Trenholm <charleshtrenholm@gmail.com>
2020-09-03 12:49:39 -0700

    Testing: update tests for app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,31 +3,68 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { JwtHelperService, JWT_OPTIONS } from '@auth0/angular-jwt';
+import { QueryBuilderService } from 'src/app/shared/services/query-builder.service';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { Spectator, createComponentFactory } from '@ngneat/spectator';
+import { Router, ActivatedRoute } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http'; 
+import { RouterModule } from '@angular/router';
+import { of } from 'rxjs';
 
-describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      providers: [
-        JwtHelperService,
-        {
-          provide: JWT_OPTIONS,
-          useValue: JWT_OPTIONS
+fdescribe('AppComponent', () => {
+
+  let spectator: Spectator<AppComponent>;
+
+  const createComponent = createComponentFactory({
+    component: AppComponent,
+    providers: [
+      JwtHelperService,
+      {
+        provide: JWT_OPTIONS,
+        useValue: JWT_OPTIONS
+      },
+      {
+        provide: QueryBuilderService,
+        useValue: {
+          resetObject() {}
         }
-      ],
-      imports: [
-        RouterTestingModule,
-        HttpClientTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
+      },
+      {
+        provide: AuthService,
+        useValue: {
+          logout() {}
+        }
+      },
+    ],
+    imports: [
+      HttpClientModule,
+      RouterModule.forRoot([])
+    ]
+  })
+
+  beforeEach(() => spectator = createComponent());
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    expect(spectator.component).toBeTruthy();
   });
 
+  it('should sign out', () => {
+    const authService = spectator.get(AuthService);
+    spyOn(spectator.component, 'logout').and.callThrough();
+    spyOn(authService, 'logout');
+    spectator.click('a#signout-nav-link');
+    spectator.detectChanges();
+
+    expect(spectator.component.logout).toHaveBeenCalled();
+    expect(authService.logout).toHaveBeenCalled();
+  });
+
+  it('should clear on search click', () => {
+    spyOn(spectator.component, 'clearPlotBuilder');
+    spectator.component.homeSearchRedirect = true;
+    spectator.click('a#search-nav-link');
+    spectator.detectChanges();
+
+    expect(spectator.component.clearPlotBuilder).toHaveBeenCalled();
+  });
 });
