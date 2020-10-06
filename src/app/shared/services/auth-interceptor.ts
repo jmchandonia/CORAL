f82f174aac49ca5b6f869e5c68cfcd1a7c4293f1
Charles Trenholm <charleshtrenholm@gmail.com>
2020-10-06 15:55:18 -0700

    Security: log out user and clear data if server responds with unauthorized status code

--- src/app/shared/services/auth-interceptor.ts
+++ src/app/shared/services/auth-interceptor.ts
@@ -4,12 +4,17 @@ import {
     HttpRequest,
     HttpHandler,
     HttpInterceptor,
+    HttpErrorResponse,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of, empty, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable()
 
 export class AuthInterceptor implements HttpInterceptor {
+
+    constructor(private router: Router) {}
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
@@ -25,7 +30,16 @@ export class AuthInterceptor implements HttpInterceptor {
             }
         });
 
-        return next.handle(request);
+        return next.handle(request).pipe(catchError(e => this.handleAuthError(e)));
+    }
+
+    handleAuthError(error: HttpErrorResponse): Observable<any> {
+        if (error.status === 401 || error.status === 403) {
+            localStorage.clear();
+            this.router.navigate(['/login']);
+            return of(empty());
+        }
+        return throwError(error);
     }
 
 }
