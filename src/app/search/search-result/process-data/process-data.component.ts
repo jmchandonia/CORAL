deb3335911e4b18a6bce7f55586689906ed0d40a
Charles Trenholm <charleshtrenholm@gmail.com>
2020-10-12 09:40:10 -0700

    Feat: remove all datatables dependencies and replace with ngx-datatables where necessary

--- src/app/search/search-result/process-data/process-data.component.ts
+++ src/app/search/search-result/process-data/process-data.component.ts
@@ -1,16 +1,14 @@
-import { Component, OnInit, Input, AfterViewInit, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectorRef } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { QueryBuilderService } from 'src/app/shared/services/query-builder.service';
-import * as $ from 'jquery';
-import 'datatables.net';
-import 'datatables.net-bs4';
+import { ColumnMode } from '@swimlane/ngx-datatable';
 
 @Component({
   selector: 'app-process-data',
   templateUrl: './process-data.component.html',
   styleUrls: ['./process-data.component.css']
 })
-export class ProcessDataComponent implements OnInit, AfterViewInit {
+export class ProcessDataComponent implements OnInit {
 
   processesDown: any[];
   processesUp: any[];
@@ -19,6 +17,7 @@ export class ProcessDataComponent implements OnInit, AfterViewInit {
   hideProcessInputs = true;
   hideProcessOutputs: boolean[] = [];
   dataTables: any;
+  columnMode = ColumnMode;
 
   constructor(
     private router: Router,
@@ -30,15 +29,6 @@ export class ProcessDataComponent implements OnInit, AfterViewInit {
   @Input() id: string;
   @Input() item: any;
 
-  ngAfterViewInit() {
-    const tables: any = $('.data-table');
-    this.dataTables = tables.DataTable({
-      ordering: false,
-      info: false
-    });
-    this.chRef.detectChanges();
-  }
-
   ngOnInit() {
     this.route.params.subscribe(params => {
       if (params.id) {
@@ -47,7 +37,7 @@ export class ProcessDataComponent implements OnInit, AfterViewInit {
       }
     });
   }
-
+  
   getProcesses() {
     this.queryBuilder.getProcessesUp(this.id)
     .subscribe((data: any) => {
