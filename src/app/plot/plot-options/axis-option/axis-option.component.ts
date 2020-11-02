1e4b95d84ee2851f8e228b1dd62e71f10571bd99
Charles Trenholm <charleshtrenholm@gmail.com>
2020-11-02 08:03:25 -0800

    Feat: enable axis to parse valid scalar types and filter out incompatible plot types

--- src/app/plot/plot-options/axis-option/axis-option.component.ts
+++ src/app/plot/plot-options/axis-option/axis-option.component.ts
@@ -1,21 +1,40 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Axis, AxisOption } from 'src/app/shared/models/plotly-builder';
+import { AxisData } from 'src/app/shared/models/plotly-config';
 
 @Component({
   selector: 'app-axis-option',
   templateUrl: './axis-option.component.html',
   styleUrls: ['./axis-option.component.css']
 })
+
 export class AxisOptionComponent implements OnInit {
 
-  @Input() axis: Axis;
-  @Input() options: AxisOption[];
+  @Input() axis: Axis; // TODO: Figure out a way to combine these 2?
+  @Input() axisValidation: AxisData;
+  private _options: AxisOption[];
+  public validOptions: AxisOption[];
+  @Input() set options(_options: AxisOption[]) {
+    this._options = _options;
+    // TODO: checking this.axisValidation in another input setter is risky
+    this.validOptions = this.axisValidation.numeric_only
+      ? this.options.filter(option => {
+        return option.scalarType === 'float' ||
+        option.scalarType === 'int' ||
+        option.scalarType === 'date';
+      })
+      : this.options;
+  }
+  get options() { return this._options }
   @Input() coreType = false;
   @Input() label: string;
 
   constructor() { }
 
   ngOnInit(): void {
+    if (this.validOptions.length === 1) {
+      this.axis.data = this.validOptions[0];
+    }
   }
 
   setAxis(event: AxisOption) {
