86033a3754cf1cb758514fb62bf7c1dfbbb6fccb
Charles Trenholm <charleshtrenholm@gmail.com>
2020-11-10 15:38:40 -0800

    Feat: add backwards navigation to map plot result

--- src/app/shared/models/map-builder.ts
+++ src/app/shared/models/map-builder.ts
@@ -1,7 +1,14 @@
 import { QueryBuilder } from 'src/app/shared/models/QueryBuilder'
 
 export class MapBuilder {
+
+    constructor(isCoreType = true) {
+        this.isCoreType = isCoreType;
+    }
+
     query: QueryBuilder;
     colorField: string = '';
     labelField: string;
+    isCoreType: boolean;
+    brickId: string;
 }
