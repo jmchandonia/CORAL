b74a7484a6f71c2d37d7cfdd1dd8caf530050dd9
Charles Trenholm <charleshtrenholm@gmail.com>
2020-11-10 13:37:25 -0800

    Feat: add coloring for map plots, additional properties for plotly

--- src/app/plot/map-result/map-result.component.ts
+++ src/app/plot/map-result/map-result.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { MapBuilder } from 'src/app/shared/models/map-builder';
 import { QueryBuilderService } from 'src/app/shared/services/query-builder.service';
+import { AgmMap, AgmInfoWindow } from '@agm/core';
 
 @Component({
   selector: 'app-map-result',
@@ -17,6 +18,12 @@ export class MapResultComponent implements OnInit {
   results: any[];
   averageLat: number;
   averageLng: number;
+  lowestScale: number;
+  highestScale: number;
+  averageScale: number;
+
+  @ViewChild('map') map: AgmMap;
+  @ViewChild('infoWindow') infoWindow: AgmInfoWindow;
 
   ngOnInit(): void {
     this.mapBuilder = JSON.parse(localStorage.getItem('mapBuilder'));
@@ -24,8 +31,39 @@ export class MapResultComponent implements OnInit {
       .subscribe(res => {
         this.averageLat = res.data.reduce((a, {latitude}) => a + latitude, 0) / res.data.length;
         this.averageLng = res.data.reduce((a, {longitude}) => a + longitude, 0) / res.data.length;
-        this.results = res.data;
-      })
-  } 
+        if (this.mapBuilder.colorField) {
+          const field = this.mapBuilder.colorField;
+          this.lowestScale = Math.min.apply(null, res.data.map(d => d[field]));
+          this.highestScale = Math.max.apply(null, res.data.map(d => d[field]));
+          this.averageScale = res.data.reduce((a, item) => a + item[field], 0) / res.data.length;
+        }
+        this.results = res.data.map(result => ({...result, scale: this.calculateScale(result)}));
+      });
+  }
+
+  calculateScale(result: any): string {
+    if (!this.mapBuilder.colorField) return '0000FF';
+
+    const totalRange = this.highestScale - this.lowestScale;
+    const field = this.mapBuilder.colorField;
+    let red = 255, blue = 255;
+                                        
+    if (result[field] < this.averageScale) {
+      blue = 255;
+      const difference = result[field] - this.lowestScale;
+      red = Math.min(Math.floor((difference / (totalRange / 2)) * 255), 255);
+    }
+
+    if (result[field] > this.averageScale) {
+      red = 255;
+      const difference = this.highestScale - result[field];
+      blue = Math.min(Math.floor((difference / (totalRange/2)) * 255), 255);
+    }
+    return `${red.toString(16).toUpperCase().padStart(2, '0')}00${blue.toString(16).toUpperCase().padStart(2, '0')}`;
+  }
+
+  getIconUrl(scale) {
+    return `http://chart.googleapis.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|${scale}|000000`
+  }
 
 }
--- src/app/plot/plot-options/map-options/map-options.component.ts
+++ src/app/plot/plot-options/map-options/map-options.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { MapBuilder } from 'src/app/shared/models/map-builder';
 import { CoreTypeAxis } from 'src/app/shared/models/core-type-plot-builder';
+import { AxisOption } from 'src/app/shared/models/plotly-builder';
 
 @Component({
   selector: 'app-map-options',
@@ -10,15 +11,7 @@ import { CoreTypeAxis } from 'src/app/shared/models/core-type-plot-builder';
 export class MapOptionsComponent implements OnInit {
 
   @Input() mapBuilder: MapBuilder;
-  // @Input() colorOptions: CoreTypeAxis[];
-  // @Input() labelOptions: CoreTypeAxis[];
-  @Input() set options(_options: CoreTypeAxis[]) {
-    this.labelOptions = _options;
-    this.colorOptions = [..._options.filter(option => option.scalar_type === 'float' || option.scalar_type === 'int')]
-  }
-
-  public labelOptions: CoreTypeAxis[];
-  public colorOptions: CoreTypeAxis[];
+  @Input() options: AxisOption[];
 
   constructor() { }
 
