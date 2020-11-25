bc2a4181bf7f650fbfce7fbc12f43b146776bd5e
Charles Trenholm <charleshtrenholm@gmail.com>
2020-11-24 16:22:59 -0800

    Fix: allow brick map flattening options to persist when user changes label options

--- src/app/shared/models/map-builder.ts
+++ src/app/shared/models/map-builder.ts
@@ -21,10 +21,30 @@ export class MapBuilder {
     setConstraints(dims: DimensionContext[]): void {
         // we only need to constraint dimensions for data vars
         // the only other option available to the user is options from the same dimension (wouldnt need constraining)
-        this.constraints = dims.map((dim, idx) => {
-            if (idx === this.dimWithCoords) return new Constraint(dim, idx, true);
-            return new Constraint(dim, idx, this.colorField?.data_variable === undefined && this.labelField?.data_variable === undefined);
-        });
+        if (!this.constraints) {
+            this.constraints = dims.map((dim, idx) => {
+                if (idx === this.dimWithCoords) return new Constraint(dim, idx, true);
+                return new Constraint(dim, idx, this.colorField?.data_variable === undefined && this.labelField?.data_variable === undefined);
+            });
+            // this.constrainingRequired = this.constraints.reduce((acc, c) => +c.disabled, 0) === 0;
+        } else {
+            if (this.labelField?.data_variable !== undefined || this.colorField?.data_variable !== undefined) {
+                // this.constraints.forEach((constraint, idx) => {
+                //     if (idx !== this.dimWithCoords) { constraint.disabled = false; }
+                // });
+                this.constraints = [
+                    ...this.constraints.map((constraint, idx) => {
+                        if (idx === this.dimWithCoords) {
+                            return {...constraint, disabled: true};
+                        }
+                        return {...constraint, disabled: false};
+                    })
+                ];
+            } else {
+                // this.constraints.forEach((constraint, idx) => constraint.disabled = true);
+                this.constraints = [...this.constraints.map(constraint => ({...constraint, disabled: true}))]
+            }
+        }
         this.constrainingRequired = this.constraints.reduce((acc, c) => +c.disabled, 0) === 0;
     }
 
