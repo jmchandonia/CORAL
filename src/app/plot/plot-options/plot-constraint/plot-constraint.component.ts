5573b6228d00f68daa2c9c1925d2d2973a6d4830
Charles Trenholm <charleshtrenholm@gmail.com>
2020-12-09 12:33:57 -0800

    Fix: get correct index value for dimension variables loaded via AJAX

--- src/app/plot/plot-options/plot-constraint/plot-constraint.component.ts
+++ src/app/plot/plot-options/plot-constraint/plot-constraint.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Constraint, ConstraintType, ConstraintVariable } from 'src/app/shared/models/plotly-builder';
+import { Constraint, ConstraintType, ConstraintVariable, UniqueValue } from 'src/app/shared/models/plotly-builder';
 import { DimensionContext } from 'src/app/shared/models/object-metadata';
 import { PlotService } from 'src/app/shared/services/plot.service';
 import { Response } from 'src/app/shared/models/response';
@@ -39,14 +39,14 @@ export class PlotConstraintComponent implements OnInit {
     }
   }
 
-  setSelectedValue(event, dimVar: ConstraintVariable) {
-    dimVar.selected_value = dimVar.unique_values.indexOf(event as never); // TODO: get rid of type never
+  setSelectedValue(event: UniqueValue, dimVar: ConstraintVariable) {
+    dimVar.selected_value = event.index;
   }
 
   handleSearch(event: any, variable: ConstraintVariable) {
     if (!this.constraint.dimension.truncate_variable_length || !event.term.length) return;
     this.plotService.getBrickDimVarValues(this.objectId, this.constraint.dim_idx, variable.dim_var_idx, event.term)
-      .subscribe((res: Response<any>) => {
+      .subscribe((res: Response<UniqueValue>) => {
         if (res.results.length < 100) {
           variable.unique_values = [...res.results];
         }
