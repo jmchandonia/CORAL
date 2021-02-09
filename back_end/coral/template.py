9e6b16f5cdb00eb0022689446c3bdf92aba2981b
Charles Trenholm <charleshtrenholm@gmail.com>
2021-02-09 15:24:39 -0800

    Chore: remove unused template methods and simplify template handling

--- back_end/coral/template.py
+++ back_end/coral/template.py
@@ -5,358 +5,7 @@ from openpyxl.styles import PatternFill, Font
 from openpyxl.utils import get_column_letter
 from contextlib import redirect_stdout 
 import xarray as xr
-
-def generate_brick_F1DM_template(brick_ds,file_name):
-
-    TITLE_FONT = Font(name='Calibri', size=14) 
-    INSTRUCTIONS_FONT = Font(name='Calibri', size=10, bold=True, italic=True)
-    DIM1_FILL = PatternFill('solid', fgColor="FFFFDD")  
-    DATA_FILL = PatternFill('solid', fgColor="FFDDDD") 
-    COLUMN_WIDTH = 18
-    dim1_row_counts = 10
-
-    book = Workbook()
-    sheet = book.active
-
-    format_name = 'F1DM'
-
-    data_vars = brick_ds['dataValues']
-    brick_dims = brick_ds['dimensions']
-
-
-    template_name = brick_ds['template_type']
-    data_type = brick_ds['type']['text']
-    data_vars = [ _get_contexted_var_name(dvar) for dvar in data_vars ] 
-
-    dims = [brick_dims[0]['type']['text']]
-    dim1_vars = []
-    for dim_var in brick_dims[0]['variables']:
-        var_name = _get_contexted_var_name(dim_var)
-        dim1_vars.append( '%s:%s' % (dims[0], var_name)  )
-
-    rows = [
-        ['Automatically generated template '],
-        ['Name', template_name],
-        ['Type', data_type],    
-        ['Dimension 1', dims[0]],
-        ['Data values', ', '.join(data_vars)],
-        [],
-        ['Instructions: fill in all colored parts of the spreadheet bellow with your data and metadata'],
-        ['@Format:%s' % format_name]
-    ]
-        
-    row = []
-    for dim1_var in dim1_vars:
-        row.append(dim1_var)
-    for data_var in data_vars:
-        row.append(data_var)
-    rows.append(row)
-        
-
-    for row in rows:
-        sheet.append(row)
-        
-    sheet['A1'].font = TITLE_FONT
-
-    sheet['A4'].fill = DIM1_FILL
-    sheet['B4'].fill = DIM1_FILL
-
-    sheet['A5'].fill = DATA_FILL
-    sheet['B5'].fill = DATA_FILL
-
-    sheet['A7'].font = INSTRUCTIONS_FONT
-    sheet['A8'].font = INSTRUCTIONS_FONT
-
-    # Color Dim 1
-    start_row = len(rows) + 1        
-    start_column = 1
-    for ri in range(start_row,start_row + dim1_row_counts):
-        for ci,_ in enumerate(dim1_vars):
-            sheet.cell(row=ri, column=ci+start_column).fill = DIM1_FILL 
-
-        
-    # Color Data    
-    start_row = len(rows) + 1       
-    start_column = len(dim1_vars) + 1    
-    for ri in range(start_row,start_row + dim1_row_counts):
-        for ci in range(start_column,start_column + len(data_vars)):
-            sheet.cell(row=ri, column=ci ).fill = DATA_FILL 
-        
-        
-    for i in range(1, len(dim1_vars) + len(data_vars) + 1 ):
-        c = get_column_letter(i)
-        sheet.column_dimensions[c].width = COLUMN_WIDTH   
-
-    book.save(file_name)    
-
-
-def parse_brick_F1DM_data(brick_ds, file_name):
-    SEARCH_FORMAT_MAX_ROW = 100
-    TEMPLATE_FORMAT = 'F1DM'
-    book = load_workbook(file_name)
-    sheet = book.active
-
-    dims_ds = brick_ds['dimensions']
-
-    template_data_vars = [ _get_contexted_var_name(dvar) for dvar in brick_ds['dataValues'] ] 
-    template_dim = dims_ds[0]['type']['text']
-    template_dim1_vars = []
-    for dim_var in dims_ds[0]['variables']:
-        var_name = _get_contexted_var_name(dim_var)
-        template_dim1_vars.append( '%s:%s' % (template_dim, var_name)  )
-
-    dims = [{
-        'size' : 0,
-        'dim_vars': []
-    }]
-    data_vars = []
-
-    template_format = None
-    start_row = -1
-    for ri in range(1,SEARCH_FORMAT_MAX_ROW):
-        value = sheet.cell(row=ri, column=1 ).value
-        if value is not None and value.startswith('@Format:'):
-            start_row = ri
-            template_format = value[len('@Format:'):]
-            break
-
-    if template_format is None:
-        raise ValueError('Can not identify the template format')
-    
-    if template_format != TEMPLATE_FORMAT:
-        raise ValueError('Wrong data format: the expected foramt is %s; found %s' % (
-            TEMPLATE_FORMAT, template_format))
-
-    start_row += 1
-    ci = 0
-
-    # Do dimension variables
-    for dim_var in template_dim1_vars:
-        ci += 1
-        header = sheet.cell(row=start_row, column=ci ).value
-        if header != dim_var:
-            raise ValueError('Wrong format: expected dimension variable %s; found %s' % (dim_var, header))
-        vals = _get_1d_column_data(sheet, start_row + 1, ci)
-        dims[0]['size'] = len(vals)
-        dims[0]['dim_vars'].append({
-            'values': vals            
-        })
-
-    # Do data variables
-    for data_var in template_data_vars:
-        ci += 1
-        header = sheet.cell(row=start_row, column=ci ).value
-        if header != data_var:
-            raise ValueError('Wrong format: expected data variable %s; found %s' % (data_var, header))
-        vals = _get_1d_column_data(sheet, start_row + 1, ci)
-        data_vars.append({
-            'values': vals                   
-        })            
-
-    _validate(dims, data_vars)
-    return {
-        'dims': dims,
-        'data_vars': data_vars
-    }
-
-def generate_brick_F2D_template(brick_ds,file_name):
-
-    TITLE_FONT = Font(name='Calibri', size=14) 
-    INSTRUCTIONS_FONT = Font(name='Calibri', size=10, bold=True, italic=True)
-    DIM1_FILL = PatternFill('solid', fgColor="FFFFDD")  
-    DIM2_FILL = PatternFill('solid', fgColor="DDFFDD")
-    DATA_FILL = PatternFill('solid', fgColor="FFDDDD") 
-    COLUMN_WIDTH = 18
-    dim1_row_counts = 10
-    dim2_row_counts = 10
-
-    FORMAT_NAME = 'F2D'
-
-
-    book = Workbook()
-    sheet = book.active
-
-    data_vars = brick_ds['dataValues']
-    brick_dims = brick_ds['dimensions']
-
-    template_name = brick_ds['template_type']
-    data_type = brick_ds['type']['text']
-    data_var = _get_contexted_var_name(data_vars[0])
-
-    dims = [brick_dims[0]['type']['text'], brick_dims[1]['type']['text']]
-    dim1_vars = []
-    for dim_var in brick_dims[0]['variables']:
-        var_name = _get_contexted_var_name(dim_var)
-        dim1_vars.append( '%s:%s' % (dims[0], var_name)  )
-
-    dim2_vars = []
-    for dim_var in brick_dims[1]['variables']:
-        var_name = _get_contexted_var_name(dim_var)
-        dim2_vars.append('%s:%s' % (dims[1], var_name) )
-
-    rows = [
-        ['Automatically generated template '],
-        ['Name', template_name],
-        ['Type', data_type],    
-        ['Dimension 1', dims[0]],
-        ['Dimension 2', dims[1]],
-        ['Data values', data_var],
-        [],
-        ['Instructions: fill in all colored parts of the spreadheet bellow with your data and metadata'],
-        ['@Format:%s' % FORMAT_NAME]
-    ]
-
-
-    for j in range(len(dim2_vars) -1 ):
-        row = []
-        for i in range(len(dim1_vars)):
-            row.append('')
-        row.append(dim2_vars[j])
-        rows.append(row)
-        
-    row = []
-    for dim1_var in dim1_vars:
-        row.append(dim1_var)
-    row.append(dim2_vars[-1])
-    rows.append(row)
-        
-
-    for row in rows:
-        sheet.append(row)
-        
-    sheet['A1'].font = TITLE_FONT
-
-    sheet['A4'].fill = DIM1_FILL
-    sheet['B4'].fill = DIM1_FILL
-
-    sheet['A5'].fill = DIM2_FILL 
-    sheet['B5'].fill = DIM2_FILL 
-
-    sheet['A6'].fill = DATA_FILL
-    sheet['B6'].fill = DATA_FILL
-
-    sheet['A8'].font = INSTRUCTIONS_FONT
-    sheet['A9'].font = INSTRUCTIONS_FONT
-
-    # Color Dim 1
-    start_row = len(rows) + 1        
-    start_column = 1
-    for ri in range(start_row,start_row + dim1_row_counts):
-        for ci,_ in enumerate(dim1_vars):
-            sheet.cell(row=ri, column=ci+start_column).fill = DIM1_FILL 
-
-    # Color Dim 2        
-    start_row = len(rows) - len(dim2_vars) + 1       
-    start_column = len(dim1_vars) + 2
-    for ri,_ in enumerate(dim2_vars):
-        for ci in range(start_column,start_column + dim2_row_counts):
-            sheet.cell(row=ri+ start_row, column=ci ).fill = DIM2_FILL 
-        
-    # Color Data    
-    start_row = len(rows) + 1       
-    start_column = len(dim1_vars) + 2    
-    for ri in range(start_row,start_row + dim1_row_counts):
-        for ci in range(start_column,start_column + dim2_row_counts):
-            sheet.cell(row=ri, column=ci ).fill = DATA_FILL 
-        
-        
-    for i in range(1, len(dim1_vars) + 2 ):
-        c = get_column_letter(i)
-        sheet.column_dimensions[c].width = COLUMN_WIDTH   
-
-    book.save(file_name)    
-
-def parse_brick_F2D_data(brick_ds, file_name):
-    SEARCH_FORMAT_MAX_ROW = 100
-    TEMPLATE_FORMAT = 'F2D'
-    book = load_workbook(file_name)
-    sheet = book.active
-
-    dims_ds = brick_ds['dimensions']
-
-    template_dim1 = dims_ds[0]['type']['text']
-    template_dim1_vars = []
-    for dim_var in dims_ds[0]['variables']:
-        var_name = _get_contexted_var_name(dim_var)
-        template_dim1_vars.append( '%s:%s' % (template_dim1, var_name)  )
-
-    template_dim2 = dims_ds[1]['type']['text']
-    template_dim2_vars = []
-    for dim_var in dims_ds[1]['variables']:
-        var_name = _get_contexted_var_name(dim_var)
-        template_dim2_vars.append( '%s:%s' % (template_dim2, var_name)  )
-
-
-    dims = [
-        {
-            'size' : 0,
-            'dim_vars': []
-        },{
-            'size' : 0,
-            'dim_vars': []
-        }]
-    data_vars = []
-
-    template_format = None
-    start_row = -1
-    for ri in range(1,SEARCH_FORMAT_MAX_ROW):
-        value = sheet.cell(row=ri, column=1 ).value
-        if value is not None and value.startswith('@Format:'):
-            start_row = ri
-            template_format = value[len('@Format:'):]
-            break
-
-    if template_format is None:
-        raise ValueError('Can not identify the template format')
-    
-    if template_format != TEMPLATE_FORMAT:
-        raise ValueError('Wrong data format: the expected foramt is %s; found %s' % (
-            TEMPLATE_FORMAT, template_format))
-
-    start_row += 1
-
-    # Do dimension1 variables
-    dim1_start_row = start_row + len(template_dim2_vars) - 1
-    dim1_ci = 0
-    for dim_var in template_dim1_vars:
-        dim1_ci += 1
-        header = sheet.cell(row=dim1_start_row, column=dim1_ci ).value
-        if header != dim_var:
-            raise ValueError('Wrong format: expected dimension variable %s; found %s' % (dim_var, header))
-        vals = _get_1d_column_data(sheet, dim1_start_row + 1, dim1_ci)
-        dims[0]['size'] = len(vals)
-        dims[0]['dim_vars'].append({
-            'values': vals            
-        })
-
-    # Do dimension2 variables
-    dim2_start_row = start_row
-    dim2_ci = len(template_dim1_vars) + 1
-    for dim_var in template_dim2_vars:
-        header = sheet.cell(row=dim2_start_row, column=dim2_ci ).value
-        if header != dim_var:
-            raise ValueError('Wrong format: expected dimension variable %s; found %s' % (dim_var, header))
-        vals = _get_1d_row_data(sheet, dim2_start_row, dim2_ci + 1)
-        dims[1]['size'] = len(vals)
-        dims[1]['dim_vars'].append({
-            'values': vals            
-        })
-        dim2_start_row += 1
-
-
-    # Do data variable
-    ri = start_row + len(template_dim2_vars)
-    ci = len(template_dim1_vars) + 1 + 1
-    vals = _get_2d_data(sheet, ri, ci, dims[0]['size'], dims[1]['size'])
-    data_vars.append({
-        'values': vals                   
-    })            
-
-    _validate(dims, data_vars)
-    return {
-        'dims': dims,
-        'data_vars': data_vars
-    }
+import pandas as pd
 
 
 def parse_brick_F2D_CSV_data(brick_ds, file_name):
@@ -498,145 +147,14 @@ def _get_contexted_var_name(dvar):
 
     return var_name if len(items) == 0 else '%s:%s' % (var_name, '; '.join(items))
 
-def generate_brick_F3D_tabs_template(brick_ds, file_name):
+def generate_brick_template(brick_ds, file_name, tab_data=False):
 
-    TITLE_FONT = Font(name='Calibri', size=14)
-    INSTRUCTIONS_FONT = Font(name='Calibri', size=10, bold=True, italic=True)
-    DIM1_FILL = PatternFill('solid', fgColor='FFFFDD')
-    DIM2_FILL = PatternFill('solid', fgColor='DDFFDD')
-    DIM3_FILL = PatternFill('solid', fgColor='DDDDFF')
-    DATA_FILL = PatternFill('solid', fgColor='FFDDDD')
-    COLUMN_WIDTH = 18
-    dim1_row_counts = 10
-    dim2_row_counts = 10
-    dim3_row_counts = 10
-    FORMAT_NAME = 'F3D'
+    """
+    generates excel template for brick with any number of dimensions.
+    generates multiple tabs within a sheet. if tab_data is True, it will tab across individual data vars,
+    otherwise will tab across the highest dimension variable
+    """
 
-    book = Workbook()
-    sheets = [book.active]
-
-    template_name = brick_ds['template_type']
-    data_vars = brick_ds['dataValues']
-    brick_dims = brick_ds['dimensions']
-    data_type = brick_ds['type']['text']
-    data_var = _get_contexted_var_name(data_vars[0])
-
-    for i in range(dim3_row_counts - 1):
-        sheets.append(book.create_sheet())
-
-    #dims = [brick_dims[0]['type']['text'], brick_dims[1]['type']['text']]
-    #dim1_vars = []
-
-    for sheet in sheets:
-        dims = [brick_dims[0]['type']['text'], brick_dims[1]['type']['text']]
-        dim1_vars = []
-        for dim_var in brick_dims[0]['variables']:
-            var_name = _get_contexted_var_name(dim_var)
-            dim1_vars.append( '%s:%s' % (dims[0], var_name))
-
-        dim2_vars = []
-        for dim_var in brick_dims[1]['variables']:
-            var_name = _get_contexted_var_name(dim_var)
-            dim2_vars.append( '%s:%s' % (dims[1], var_name))
-
-        dim3_vars = []
-        for dim_var in brick_dims[2]['variables']:
-            var_name = _get_contexted_var_name(dim_var)
-            dim3_vars.append('%s:%s' % (brick_dims[2]['type']['text'], var_name))
-
-        rows = [
-            ['Automatically generated template '],
-            ['Name', template_name],
-            ['Type', data_type],
-            ['Dimension 1', dims[0]],
-            ['Dimension 2', dims[1]],
-            ['Dimension 3', brick_dims[2]['type']['text']],
-            ['Data values', data_var],
-            [],
-            ['Instructions: fill in all colored parts of the spreadsheet below with your data and metadata'],
-            ['@Format:%s' % FORMAT_NAME],
-            []
-        ]
-
-        dim3_row_labels = []
-        dim3_rows = []
-
-        for i in dim3_vars:
-           dim3_row_labels.append(i)
-           dim3_rows.append('')
-
-        rows.append(dim3_row_labels)
-        rows.append(dim3_rows)
-
-        for j in range(len(dim2_vars) - 1):
-            row = []
-            for i in range(len(dim1_vars)):
-                row.append('')
-            row.append(dim2_vars[j])
-            rows.append(row)
-
-        row = []
-        for dim1_var in dim1_vars:
-            row.append(dim1_var)
-        row.append(dim2_vars[-1])
-        rows.append(row)
-
-        for row in rows:
-            sheet.append(row)
-
-        sheet['A1'].font = TITLE_FONT
-
-        sheet['A4'].fill = DIM1_FILL
-        sheet['B4'].fill = DIM1_FILL
-
-        sheet['A5'].fill = DIM2_FILL
-        sheet['B5'].fill = DIM2_FILL
-
-        sheet['A6'].fill = DIM3_FILL
-        sheet['B6'].fill = DIM3_FILL
-
-        sheet['A7'].fill = DATA_FILL
-        sheet['B7'].fill = DATA_FILL
-
-        sheet['A9'].font = INSTRUCTIONS_FONT
-        sheet['A10'].font = INSTRUCTIONS_FONT
-
-        #TODO: color dim 3
-
-        #Color Dim 1
-        start_row = len(rows) + 1
-        start_column = 1
-        for ri in range(start_row, start_row + dim1_row_counts):
-            for ci,_ in enumerate(dim1_vars):
-                sheet.cell(row=ri, column=ci+start_column).fill = DIM1_FILL
-
-        # Color Dim 3
-        start_row = len(rows) - 1
-        for ci, _ in enumerate(dim3_vars):
-            sheet.cell(row=start_row - 1, column=ci + start_column).fill = DIM3_FILL
-            sheet.cell(row=start_row, column=ci + start_column).fill = DIM3_FILL
-
-        #Color Dim 2
-        start_row = len(rows) - len(dim2_vars) + 1
-        start_column = len(dim1_vars) + 2
-        for ri, _ in enumerate(dim2_vars):
-            for ci in range(start_column, start_column + dim2_row_counts):
-                sheet.cell(row=ri + start_row, column=ci).fill = DIM2_FILL
-
-        # Color Data
-        start_row = len(rows) + 1
-        start_column = len(dim1_vars) + 2
-        for ri in range(start_row, start_row + dim1_row_counts):
-            for ci in range(start_column, start_column + dim2_row_counts):
-                sheet.cell(row=ri, column=ci).fill = DATA_FILL
-
-        for i in range(1, len(dim1_vars) + 2):
-            c = get_column_letter(i)
-            sheet.column_dimensions[c].width = COLUMN_WIDTH
-
-    book.save(file_name)
-        
-def generate_brick_FND_template(brick_ds, file_name, tab_data=False):
     TITLE_FONT = Font(name='Calibri', size=14)
     INSTRUCTIONS_FONT = Font(name='Calibri', size=10, bold=True, italic=True)
     DIM1_FILL = PatternFill('solid', fgColor='FFFFDD')
@@ -919,7 +437,12 @@ def generate_brick_FND_template(brick_ds, file_name, tab_data=False):
 
     book.save(file_name)
 
-def generate_brick_FND_interlace_template(brick_ds, file_name):
+def generate_brick_interlace_template(brick_ds, file_name):
+
+    """
+    generates template with all dim vars and data vars interlaced on one page (no tabs)
+    """
+
     TITLE_FONT = Font(name='Calibri', size=14)
     INSTRUCTIONS_FONT = Font(name='Calibri', size=10, bold=True, italic=True)
     COLUMN_WIDTH = 18
@@ -1131,7 +654,7 @@ def generate_brick_FND_interlace_template(brick_ds, file_name):
 
     book.save(file_name)
 
-def parse_brick_FND_data(brick_ds, file_name):
+def parse_brick_data(brick_ds, file_name):
     SEARCH_FORMAT_MAX_ROW = 100
     book = load_workbook(file_name)
     sheet = book.active
@@ -1146,15 +669,15 @@ def parse_brick_FND_data(brick_ds, file_name):
         raise ValueError('Cannot identify the template format')
 
     if template_format == 'FND_INTERLACE':
-        return parse_brick_FND_interlace_data(brick_ds, sheet)
+        return parse_brick_interlace_data(brick_ds, sheet)
     elif template_format == 'FND_TAB_DIM':
-        return parse_brick_FND_tab_dim(brick_ds, book)
+        return parse_brick_tab_dim(brick_ds, book)
     elif template_format == 'FND_TAB_DATA':
-        return parse_brick_FND_tab_data(brick_ds, book)
+        return parse_brick_tab_data(brick_ds, book)
     else:
         raise ValueError('Wrong data format: the expected format is FND_INTERLACE, FND_TAB_DIM, or FND_TAB_DATA; found %s' % template_format)
 
-def parse_brick_FND_interlace_data(brick_ds, sheet):
+def parse_brick_interlace_data(brick_ds, sheet):
 
     dims_ds = brick_ds['dimensions']
 
@@ -1268,7 +791,7 @@ def parse_brick_FND_interlace_data(brick_ds, sheet):
         'data_vars': data_vars
     }
 
-def parse_brick_FND_tab_data(brick_ds, book):
+def parse_brick_tab_data(brick_ds, book):
     dims_ds = brick_ds['dimensions']
     template_dims = [dim['type']['text'] for dim in dims_ds]
     template_dim_vars = []
@@ -1379,7 +902,7 @@ def parse_brick_FND_tab_data(brick_ds, book):
         'data_vars': data_vars
     }
 
-def parse_brick_FND_tab_dim(brick_ds, book):
+def parse_brick_tab_dim(brick_ds, book):
     dims_ds = brick_ds['dimensions']
 
     template_dims = [dim['type']['text'] for dim in dims_ds]
