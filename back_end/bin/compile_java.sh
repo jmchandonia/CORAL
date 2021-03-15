46ab93071072a6fb4ed122c490c44884d2693383
Charles Trenholm <charleshtrenholm@gmail.com>
2021-03-15 14:43:37 -0700

    Fix: add extra packages to java classpath

--- back_end/bin/compile_java.sh
+++ back_end/bin/compile_java.sh
@@ -7,7 +7,7 @@
 
 PROJECT_ROOT="$(grep -o '"project_root": "[^"]*' ../back_end/python/var/config.json | grep -o '[^"]*$')"
 
-CLASSPATH="${PROJECT_ROOT}/java/classes:${PROJECT_ROOT}/java/GenericsUtil/lib/src:${PROJECT_ROOT}/java/jars/lib/jars/jackson/jackson-core-2.5.4.jar:${PROJECT_ROOT}/java/jars/lib/jars/jackson/jackson-annotations-2.5.4.jar:${PROJECT_ROOT}/java/jars/lib/jars/jackson/jackson-databind-2.5.4.jar:${PROJECT_ROOT}/java/jars/lib/jars/jackson/jackson-jaxrs-base-2.5.4.jar:${PROJECT_ROOT}/java/jars/lib/jars/jackson/jackson-jaxrs-json-provider-2.5.4.jar:${PROJECT_ROOT}/java/jars/lib/jars/jackson/jackson-module-jaxb-annotations-2.5.4.jar:${PROJECT_ROOT}/java/jars/lib/jars/opencsv/opencsv-3.9.jar:${PROJECT_ROOT}/java/jars/lib/jars/annotation/javax.annotation-api-1.3.2.jar:${PROJECT_ROOT}/java/jars/lib/jars/strbio/strbio-1.3.jar"
+CLASSPATH="${PROJECT_ROOT}/java/classes:${PROJECT_ROOT}/java/GenericsUtil/lib/src:${PROJECT_ROOT}/java/jars/lib/jars/jackson/jackson-core-2.5.4.jar:${PROJECT_ROOT}/java/jars/lib/jars/jackson/jackson-annotations-2.5.4.jar:${PROJECT_ROOT}/java/jars/lib/jars/jackson/jackson-databind-2.5.4.jar:${PROJECT_ROOT}/java/jars/lib/jars/jackson/jackson-jaxrs-base-2.5.4.jar:${PROJECT_ROOT}/java/jars/lib/jars/jackson/jackson-jaxrs-json-provider-2.5.4.jar:${PROJECT_ROOT}/java/jars/lib/jars/jackson/jackson-module-jaxb-annotations-2.5.4.jar:${PROJECT_ROOT}/java/jars/lib/jars/opencsv/opencsv-3.9.jar:${PROJECT_ROOT}/java/jars/lib/jars/annotation/javax.annotation-api-1.3.2.jar:${PROJECT_ROOT}/java/jars/lib/jars/strbio/strbio-1.3.jar:${PROJECT_ROOT}/java/jars/lib/jars/apache_commons/commons-lang3-3.5.jar"
 export CLASSPATH
 
 javac "$PROJECT_ROOT"/java/classes/gov/lbl/enigma/app/*.java
