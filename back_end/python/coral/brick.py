22a39bf76a804fd17af162d260d2e66d771403f2
John-Marc Chandonia <JMChandonia@lbl.gov>
2023-03-03 14:31:38 -0800

    Fixed deprecated numpy data type

--- back_end/python/coral/brick.py
+++ back_end/python/coral/brick.py
@@ -19,7 +19,7 @@ class NPEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, np.integer):
             return int(obj)
-        elif isinstance(obj, np.float):
+        elif isinstance(obj, np.floating):
             return float(obj)
         elif isinstance(obj, np.ndarray):
             return obj.tolist()
