30c9d5880e00344108a09489376c1a0376d19d0f
John-Marc Chandonia <JMChandonia@lbl.gov>
2025-01-29 18:14:06 -0800

    Updated microtype browser to remove ENIGMA dependencies

--- front_end/src/app/shared/services/microtype-tree-factory.service.ts
+++ front_end/src/app/shared/services/microtype-tree-factory.service.ts
@@ -17,11 +17,10 @@ export class MicrotypeTreeFactoryService {
       microtype.children = [];
       // filter out ids with parents to roots, results in double values otherwise
       microtype.mt_parent_term_ids = microtype.mt_parent_term_ids
-        .filter(id => id !== 'ENIGMA:0000000' && id !== 'ME:0000000');
+        .filter(id => !id.endsWith(':0000000'));
 
       microtype.mt_parent_term_ids.forEach(id => {
-        // make sure first item in array (ME:0000039) gets added to list
-        if ((map[id] || id === 'ME:0000039')) {
+        if (id in map) {
           microtypes[map[id]].children.push(microtype);
         } else {
           result.push(microtype);
